7f2e86da05f9878428041ca70177ece985aa3e9b
md.shamim <imsiddiquee@gmail.com>
2016-01-06 14:52:19 +0600

    home model create

---
+++ SmartHome.Model/Models/Home.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHome.Model.Models
+{
+    public class Home
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int HomeId { get; set; }
+        public string Name { get; set; }        
+        public string Zone { get; set; }
+        public string Block { get; set; }
+        public string TimeZone { get; set; }
+        public string RegistrationKey { get; set; }
+        public string HardwareId { get; set; }
+        public int TrialCount { get; set; }
+        public AuditFields AuditField { get; set; }
+        public string Comment { get; set; }
+        public bool IsActive { get; set; }
+
+        public virtual ICollection<Address> Addresses { get; set; }
+        public virtual ICollection<HomeVersion> HomeVersions { get; set; }
+        public virtual ICollection<HomeStatus> HomeStatuses { get; set; }
+        public virtual ICollection<CommType> CommTypes { get; set; }
+        public virtual ICollection<HomeLink> HomeLinks { get; set; }
+        public virtual ICollection<Room> Rooms { get; set; }
+    }
+}
