08f0f04fda740f13633687516497d79ac645ec7d
md.shamim <imsiddiquee@gmail.com>
2016-01-06 14:43:00 +0600

    address model create

---
+++ SmartHome.Model/Models/Address.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHome.Model.Models
+{
+
+    public class Address
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int AddressId { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string OfficePhone { get; set; }
+        public string WorkPhone { get; set; }
+        public string ZipCode { get; set; }
+        public AuditFields AuditField { get; set; }
+        public virtual Home Home { get; set; }
+        public virtual UserProfile UserProfile { get; set; }
+
+    }
+}
