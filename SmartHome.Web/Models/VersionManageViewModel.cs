4986f68ab0cdf5cdae87c2128553defa1a50a6ef
md.shamim <imsiddiquee@gmail.com>
2016-01-07 09:13:55 +0600

    version view model create

---
+++ SmartHome.Web/Models/VersionManageViewModel.cs
@@ -0,0 +1,38 @@
+﻿
+using SmartHome.Model.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SmartHome.Web.Models
+{
+    public class VersionManageViewModel:VersionViewModel
+    {
+        public VersionManageViewModel()
+            : base()
+        {
+        }
+        public VersionManageViewModel(Version model)
+            : base(model)
+        {
+        }
+
+        public Version ToDalEntity()
+        {
+            return ToDalEntity(new Version());
+        }
+
+        public Version ToDalEntity(Version model)
+        {
+            model.VersionId = this.VersionId;
+            model.Id = this.Id;
+            model.AppName = this.AppName;
+            model.AppVersion = this.AppVersion;
+            model.AuthCode = this.AuthCode;
+            model.PassPhrase = this.PassPhrase;
+            model.MAC = this.MAC;
+            return model;
+        }
+
+    }
+}
---
+++ SmartHome.Web/Models/VersionViewModel.cs
@@ -0,0 +1,25 @@
+﻿using SmartHome.Model.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SmartHome.Web.Models
+{
+    public class VersionViewModel:Version
+    {
+        public VersionViewModel()
+        {
+        }
+
+        public VersionViewModel(Version model)
+        {
+            this.VersionId = model.VersionId;
+            this.Id = model.Id;
+            this.AppName = model.AppName;
+            this.AppVersion = model.AppVersion;
+            this.AuthCode = model.AuthCode;
+            this.PassPhrase = model.PassPhrase;
+            this.MAC = model.MAC;
+        }
+    }
+}
