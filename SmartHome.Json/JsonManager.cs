48ad7596a6d994523a763931f0e719629aad0526
md.shamim <imsiddiquee@gmail.com>
2016-01-11 13:49:48 +0600

    Json Process for version and version detail

--- SmartHome.Json/JsonManager.cs
+++ SmartHome.Json/JsonManager.cs
@@ -21,59 +21,72 @@ namespace SmartHome.Json
     {
         public static void JsonProcess(string JsonString)
         {
-
-            #region MyRegion
-
-            RootObject myObj = JsonConvert.DeserializeObject<RootObject>(JsonString);
-            Mapper.CreateMap<RootObject, Model.Models.Version>();
-
-             // Mapper.Map<IEnumerable<myObj.Version>, IEnumerable<Model.Models.Version>>(syncList);
-            foreach (var item in myObj.Version)
+            try
             {
-                // Model.Models.Version model = Mapper.Map<Entity.Version, Model.Models.Version>(item);
+                RootObject oRootObject = JsonDesrialized(JsonString);
+                IEnumerable<Model.Models.Version> oVersion = ConfigureVersion(oRootObject);
+                IEnumerable<Model.Models.VersionDetail> oVersionDetail = ConfigureVersionDetail(oRootObject);
+                StoreVersionAndVersionDetail(oVersion, oVersionDetail);
+            }
+            catch (Exception ex)
+            {
 
+                throw;
+            }
+        }
 
+        private static RootObject JsonDesrialized(string JsonString)
+        {
+            return JsonConvert.DeserializeObject<RootObject>(JsonString);
+        }
+
+        private static void StoreVersionAndVersionDetail(IEnumerable<Model.Models.Version> oVersion, IEnumerable<Model.Models.VersionDetail> oVersionDetail)
+        {
+            foreach (var item in oVersion)
+            {
                 using (IDataContextAsync context = new SmartHomeDataContext())
                 using (IUnitOfWorkAsync unitOfWork = new UnitOfWork(context))
                 {
+
                     IRepositoryAsync<Model.Models.Version> versionRepository = new Repository<Model.Models.Version>(context, unitOfWork);
-
-                    var Version = new Model.Models.Version
-                    {
-                        AppName = item.AppName,
-                        AppVersion = item.AppVersion,
-                        Id = item.ID,
-                        AuthCode=item.AuthCode,
-                        PassPhrase=item.PassPhrase,
-                        AuditField = new AuditFields(),
-                        ObjectState = ObjectState.Added
-                    };
-                    versionRepository.Insert(Version);
+                    item.VersionDetails = oVersionDetail.Where(p => p.VId == item.Id).ToArray();
+                    versionRepository.InsertOrUpdateGraph(item);
                     unitOfWork.SaveChanges();
+
                 }
-
             }
+        }
 
+        private static IEnumerable<Model.Models.VersionDetail> ConfigureVersionDetail(RootObject myObj)
+        {
+            Mapper.CreateMap<Entity.VersionDetail, Model.Models.VersionDetail>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.ID))
+            .ForMember(dest => dest.AuditField, opt => opt.UseValue(new AuditFields()))
+            .ForMember(dest => dest.ObjectState, opt => opt.UseValue(ObjectState.Added))//state
+            .ForMember(dest => dest.VId, opt => opt.MapFrom(src => src.VersionID));
+            IEnumerable<Model.Models.VersionDetail> oVersionDetail = Mapper.Map<IEnumerable<Entity.VersionDetail>, IEnumerable<Model.Models.VersionDetail>>(myObj.VersionDetails);
+            return oVersionDetail;
+        }
 
+        private static IEnumerable<Model.Models.Version> ConfigureVersion(RootObject myObj)
+        {
+            Mapper.CreateMap<Entity.Version, Model.Models.Version>()
+                        .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.ID))
+                        .ForMember(dest => dest.AuditField, opt => opt.UseValue(new AuditFields()))//audit field
+                        .ForMember(dest => dest.ObjectState, opt => opt.UseValue(ObjectState.Added));//state
+            IEnumerable<Model.Models.Version> oVersion = Mapper.Map<IEnumerable<Entity.Version>, IEnumerable<Model.Models.Version>>(myObj.Version);
+            return oVersion;
+        }
 
-            // Create new customer
-            //using (IDataContextAsync context = new SmartHomeDataContext())
-            //using (IUnitOfWorkAsync unitOfWork = new UnitOfWork(context))
-            //{
-            //    IRepositoryAsync<Model.Models.Version> versionRepository = new Repository<Model.Models.Version>(context, unitOfWork);
+        public static TDestination Resolve<TSource, TDestination>(TSource source)
+        {
+            var mapped = Mapper.FindTypeMapFor(typeof(TSource), typeof(TDestination)); //this will give you a reference to existing mapping if it was created or NULL if not
 
-            //    var Version = new Model.Models.Version
-            //    {
-            //        AppName = "App 3",
-            //        AppVersion = "1.3",
-            //        AuditField = new AuditFields(),
-            //        ObjectState = ObjectState.Added
-            //    };
-
-            //    versionRepository.Insert(Version);
-            //    unitOfWork.SaveChanges();
-            //}
-            #endregion
+            if (mapped == null)
+            {
+                var expression = Mapper.CreateMap<TSource, TDestination>();
+            }
+            return Mapper.Map<TSource, TDestination>(source);
         }
     }
 }
