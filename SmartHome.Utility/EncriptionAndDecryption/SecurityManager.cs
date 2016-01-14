f97ba6bb018a071e8d20a7ded90b5d83efba0168
md.shamim <imsiddiquee@gmail.com>
2016-01-14 12:24:06 +0600

    SmartHome.Utility security content add

---
+++ SmartHome.Utility/EncriptionAndDecryption/SecurityManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHome.Utility.EncriptionAndDecryption
+{
+    public static class SecurityManager
+    {
+        static SecurityManager()
+        {
+            RandomString = CryptLib.GenerateRandomIV(16);
+            CypherKey = CryptLib.getHashSha256("my secret key", 31);
+        }
+
+        public static string Encrypt(string plainText)
+        {
+            return new CryptLib().encrypt(plainText, CypherKey, RandomString);
+        }
+
+        public static string Decrypt(string cypherText)
+        {
+            return new CryptLib().decrypt(cypherText, CypherKey, RandomString);
+        }
+        #region General properties
+
+        private static string RandomString { get; set; }
+        private static string CypherKey { get; set; }
+        #endregion
+    }
+}
