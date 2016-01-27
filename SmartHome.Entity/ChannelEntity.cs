adaf59e19d81b69bfbd74a6c333d3aff111b7830
md.shamim <imsiddiquee@gmail.com>
2016-01-27 09:40:56 +0600

    model update

--- SmartHome.Entity/ChannelEntity.cs
+++ SmartHome.Entity/ChannelEntity.cs
@@ -10,11 +10,11 @@ namespace SmartHome.Entity
     public class ChannelEntity
     {
         #region Primitive Properties
-        
+
         public int ChannelId { get; set; }
         [JsonProperty("ID")]
         public int Id { get; set; }
-        [JsonProperty("DeviceID")]        
+        [JsonProperty("DeviceID")]
         public int DId { get; set; }
         public int ChannelNo { get; set; }
         public string LoadName { get; set; }
