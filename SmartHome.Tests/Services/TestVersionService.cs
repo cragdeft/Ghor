a0e7cbddfdd16528ae2d32b91acc33e953520285
md.shamim <imsiddiquee@gmail.com>
2016-01-31 14:20:07 +0600

    unit test version service update

--- SmartHome.Tests/Services/TestVersionService.cs
+++ SmartHome.Tests/Services/TestVersionService.cs
@@ -3,6 +3,7 @@ using Moq;
 using Repository.Pattern.Infrastructure;
 using Repository.Pattern.Repositories;
 using Repository.Pattern.UnitOfWork;
+using SmartHome.Entity;
 using SmartHome.Model.Models;
 using SmartHome.Service;
 using SmartHome.Service.Interfaces;
@@ -29,15 +30,15 @@ namespace SmartHome.Tests.Services
             _service = new VersionService(_mockRepository.Object);
 
             listVersion = new List<Model.Models.Version>() {
-             new Model.Models.Version() { AppName="App 1",ObjectState=ObjectState.Added,AuditField=new AuditFields()},
-             new Model.Models.Version() { AppName="App 2",AppVersion="1.0",ObjectState=ObjectState.Added,AuditField=new AuditFields()},
-             new Model.Models.Version() { AppName="App 3",ObjectState=ObjectState.Added,AuditField=new AuditFields()}
+             new Model.Models.Version() { VersionId=1,AppName="App 1",AppVersion="1.0",ObjectState=ObjectState.Added,AuditField=new AuditFields()},
+             new Model.Models.Version() { VersionId=1, AppName="App 2",AppVersion="1.0",ObjectState=ObjectState.Added,AuditField=new AuditFields()},
+             new Model.Models.Version() { VersionId=1,AppName="App 3",ObjectState=ObjectState.Added,AuditField=new AuditFields()}
             };
         }
 
 
         [TestMethod]
-        public void Version_Service_Get_All()
+        public void Version_Service_Gets_Async()
         {
             //Arrange            
             _mockRepository.Setup(x => x.Query().SelectAsync()).Returns(Task.FromResult((IEnumerable<Model.Models.Version>)listVersion));
@@ -50,6 +51,89 @@ namespace SmartHome.Tests.Services
                 Assert.AreEqual(executionResult.Count(), 3);
             }).GetAwaiter().GetResult();
         }
+
+
+        [TestMethod]
+        public void Version_Service_Get_Async()
+        {
+            //Arrange            
+            _mockRepository.Setup(x => x.FindAsync(1)).Returns(Task.FromResult((Model.Models.Version)listVersion.First()));
+
+            //Act
+            Task.Run(async () =>
+            {
+                var executionResult = await _service.GetAsync(1);
+
+                //Assert                
+                Assert.IsNotNull(executionResult);
+                Assert.AreEqual(executionResult.AppName, "App 1");
+
+            }).GetAwaiter().GetResult();
+        }
+
+        [TestMethod]
+        public void Can_Add_Version()
+        {
+            //Arrange
+            int versionId = 1;
+            VersionEntity version = new VersionEntity() { VersionId = versionId, AppVersion = "App1" };
+
+            //Act
+            _service.Add(version);
+
+            //Assert
+            Assert.AreEqual(version.VersionId, versionId);
+        }
+
+
+        [TestMethod]
+        public void Can_Add_Invalid_Version()
+        {
+            //Arrange
+            int versionId = 1;
+            VersionEntity version = new VersionEntity() { AppVersion = "App1" };
+
+            //Act
+            _service.Add(version);
+
+            //Assert
+            Assert.AreEqual(version.VersionId, versionId);
+        }
+
+
+        public void Version_Service_Modify()
+        {
+            //Arrange
+            int versionId = 1;
+
+            var version = listVersion.First();
+
+            //Act
+            _service.Update(version);
+
+            //Assert
+          
+            Assert.AreEqual(version.VersionId, versionId);
+        }
+
+        public void Version_Service_Delete()
+        {
+            //Arrange
+            int versionId = 1;
+
+            var version = listVersion.First();
+
+            //Act
+            _service.Delete(version);
+
+            //Assert
+
+            Assert.AreEqual(version.VersionId, versionId);
+        }
+
+
+        
+
 
     }
 }
