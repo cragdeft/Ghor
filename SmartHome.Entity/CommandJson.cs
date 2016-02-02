e80bc9f95257e38917afdbc500d0a37776ea9af8
sumon.kumar <sumon.kumar@aplombtechbd.com>
2016-02-02 12:38:15 +0600

    add new class

---
+++ SmartHome.Entity/CommandJson.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHome.Entity
+{
+    public class CommandJson
+    {
+        public String Command { get; set; }
+        public string DeviceID { get; set; }
+        public string DeviceUUID { get; set; }
+        public string Response { get; set; }
+        public string DeviceVersion { get; set; }
+        public string MacID { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
