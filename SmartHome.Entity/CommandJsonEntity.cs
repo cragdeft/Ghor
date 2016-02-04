04be957e5c1bda9ea8afd8ece7742cdcc7a43ce7
sumon.kumar <sumon.kumar@aplombtechbd.com>
2016-02-04 11:07:48 +0600

    Add new entity

---
+++ SmartHome.Entity/CommandJsonEntity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using SmartHome.Model.Enums;
+using CommandType = SmartHome.Model.Enums.CommandType;
+
+namespace SmartHome.Entity
+{
+    public class CommandJsonEntity
+    {
+        [JsonProperty("CommandJsonId")]
+        public int CommandJsonId { get; set; }
+        public int CommandID { get; set; }
+        public String Command { get; set; }
+        public int DeviceID { get; set; }
+        public int DeviceUUID { get; set; }
+        public bool Response { get; set; }
+        public string DeviceVersion { get; set; }
+        public string MacID { get; set; }
+        public string EmailAddress { get; set; }
+
+        public bool IsProcessed { get; set; }
+        public CommandType CommandType { get; set; }
+    }
+}
