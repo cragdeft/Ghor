79f856192d3ee55d0afd7f32f2d713cd55d1d3e1
sumon.kumar <sumon.kumar@aplombtechbd.com>
2016-02-04 11:10:28 +0600

    add new entity

---
+++ SmartHome.Model/Models/CommandJson.cs
@@ -0,0 +1,37 @@
+﻿using Repository.Pattern.Ef6;
+using SmartHome.Model.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHome.Model.Models
+{
+    public class CommandJson : Entity
+    {
+        #region Primitive Properties
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CommandID { get; set; }
+        public String Command { get; set; }
+        public int DeviceID { get; set; }
+        public int DeviceUUID { get; set; }
+        public bool Response { get; set; }
+        public string DeviceVersion { get; set; }
+        public string MacID { get; set; }
+        public string EmailAddress { get; set; }
+
+        public bool IsProcessed { get; set; }
+        public CommandType CommandType { get; set; }
+        #endregion
+
+        #region Complex Properties
+        public AuditFields AuditField { get; set; }
+        #endregion
+
+    }
+
+}
