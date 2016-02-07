e3819013e4f3a557f258e7334da43bf49f332370
md.shamim <imsiddiquee@gmail.com>
2016-02-07 11:27:18 +0600

    update manager service

---
+++ SmartHome.Service/Interfaces/IConfigurationParserManagerService.cs
@@ -0,0 +1,17 @@
+﻿using SmartHome.Entity;
+using SmartHome.Model.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHome.Service.Interfaces
+{
+    public interface IConfigurationParserManagerService
+    {
+        IEnumerable<Version> AddOrUpdateVersionGraphRange(IEnumerable<Version> model);
+
+        IEnumerable<Device> AddOrUpdateDeviceGraphRange(IEnumerable<Device> model);
+       // IEnumerable<DeviceInfoEntity> GetsDeviceAllInfo();
+    }
+}
