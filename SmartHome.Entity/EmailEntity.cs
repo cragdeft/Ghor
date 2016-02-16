4f209f6f19e221ba6626e2dbec4156369da52839
sumon.kumar <sumon.kumar@aplombtechbd.com>
2016-02-16 14:45:27 +0600

    add new entity

---
+++ SmartHome.Entity/EmailEntity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace SmartHome.Entity
+{
+    public class EmailEntity
+    {
+        #region Primitive Properties
+        [EmailAddress]
+        public string FromAddress { get; set; }
+        [EmailAddress]
+        public string ToAddress { get; set; }
+        public string Subject { get; set; }
+        [AllowHtml]
+        public string Body { get; set; }
+        public bool EnableSSL { get; set; }
+        public bool IsBodyHtml { get; set; }
+        public DateTime SentDate { get; set; }
+
+        public EmailEntity()
+        {
+            this.FromAddress = "smarthome-noreply@sinepulse.com";
+        }
+        #endregion
+    }
+}
