018fa726f8c633d9faa51e7daf0f5499a0fe1525
sumon.kumar <sumon.kumar@aplombtechbd.com>
2016-02-17 10:50:41 +0600

    change in methods

--- SmartHome.MailApi/Controllers/EmailController.cs
+++ SmartHome.MailApi/Controllers/EmailController.cs
@@ -8,6 +8,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using SmartHome.Entity;
 using SmartHome.MailApi.MailHelper;
+using SmartHome.Utility.EncriptionAndDecryption;
 
 namespace SmartHome.MailApi.Controllers
 {
@@ -15,20 +16,27 @@ namespace SmartHome.MailApi.Controllers
     {
         // GET: Email
         [System.Web.Mvc.Route("api/SendMail")]
-        public HttpResponseMessage SendMail(EmailEntity email, bool isEncrypted)
+        public HttpResponseMessage SendMailFromEncryptedJson(string mailJsonString)
         {
+            var email = SetEmailEntity(mailJsonString);
             HttpResponseMessage response;
             if (ModelState.IsValid)
             {
                 SmartHomeMailClient mailClient = new SmartHomeMailClient(email);
-                response = Request.CreateResponse(HttpStatusCode.OK, mailClient.SendEmail(isEncrypted));
+                response = Request.CreateResponse(HttpStatusCode.OK, mailClient.SendEmail());
             }
             else
-            {
                 response = Request.CreateResponse(HttpStatusCode.OK, false);
-            }
 
             return response;
+        }
+
+        private EmailEntity SetEmailEntity(string mailJsonString)
+        {
+            mailJsonString = SecurityManager.Decrypt(mailJsonString);
+            EmailEntity email = new EmailEntity();
+            email.SetValue(mailJsonString);
+            return email;
         }
     }
 }
