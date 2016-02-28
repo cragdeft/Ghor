f96aaa5786139c80db0f724d662d11d2d1453641
md.shamim <imsiddiquee@gmail.com>
2016-02-28 15:19:18 +0600

    spelling fix encryption

--- SmartHome.Utility/EncryptionAndDecryption/CryptLib.cs
+++ SmartHome.Utility/EncryptionAndDecryption/CryptLib.cs
@@ -2,7 +2,7 @@ using System;
 using System.Text;
 using System.Security.Cryptography;
 
-namespace SmartHome.Utility.EncriptionAndDecryption
+namespace SmartHome.Utility.EncryptionAndDecryption
 {
 	/*****************************************************************
 	 * CrossPlatform CryptLib
--- SmartHome.Utility/EncryptionAndDecryption/SecurityManager.cs
+++ SmartHome.Utility/EncryptionAndDecryption/SecurityManager.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SmartHome.Utility.EncriptionAndDecryption
+namespace SmartHome.Utility.EncryptionAndDecryption
 {
     public static class SecurityManager
     {
