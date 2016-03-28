eee748165970ef6b28619bb06b37e0b23067cea2
md.shamim <imsiddiquee@gmail.com>
2016-03-28 11:57:16 +0600

    User link model update

--- SmartHome.Model/Models/UserHomeLink.cs
+++ SmartHome.Model/Models/UserHomeLink.cs
@@ -11,18 +11,16 @@ namespace SmartHome.Model.Models
 {
     public class UserHomeLink: Entity
     {
-        //[Key, Column(Order = 0)]
-        //public int HId { get; set; }
-        //[Key, Column(Order = 1)]
-
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int UserHomeLinkId { get; set; }
-        public string HId { get; set; }
-        public string UInfoId { get; set; }
+        [Key, Column(Order = 0)]
+        public int HId { get; set; }
+        [Key, Column(Order = 1)]
+        public int UInfoId { get; set; }
         public virtual Home Home { get; set; }
         public virtual UserInfo UserInfo { get; set; }
         public bool IsAdmin { get; set; }
         public bool IsSynced { get; set; }
+
+
+       
     }
 }
