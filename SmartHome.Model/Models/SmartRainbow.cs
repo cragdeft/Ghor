57f07910eab9908ffcb6448a1a38fafb88bfcdcf
md.shamim <imsiddiquee@gmail.com>
2016-05-09 12:06:44 +0600

    add model

---
+++ SmartHome.Model/Models/SmartRainbow.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHome.Model.Models
+{
+    public class SmartRainbow : SmartDevice
+    {
+        #region  Navigation Properties
+
+        //public string TestName { get; set; }
+
+        public virtual ICollection<RgbwStatus> RgbwStatuses { get; set; }
+        #endregion
+    }
+}
---
+++ SmartHome.Model/Models/SmartSwitch.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHome.Model.Models
+{
+    public class SmartSwitch : SmartDevice
+    {
+        #region  Navigation Properties      
+        public virtual ICollection<Channel> Channels { get; set; }
+      
+        #endregion
+    }
+}
