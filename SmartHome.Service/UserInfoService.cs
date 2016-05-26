d11f316cb4e3d7697d253c1919cea677efd8773e
md.shamim <imsiddiquee@gmail.com>
2016-05-26 10:56:38 +0600

    update

--- SmartHome.Service/UserInfoService.cs
+++ SmartHome.Service/UserInfoService.cs
@@ -28,34 +28,29 @@ namespace SmartHome.Service
             _channelRepository = unitOfWork.RepositoryAsync<Channel>();
             _webPagesRoleRepository = unitOfWork.RepositoryAsync<WebPagesRole>();
         }
+
         public IEnumerable<UserInfo> GetsUserInfos()
         {
             return _userInfoRepository.Query().Select();
         }
-        public IEnumerable<UserInfo> GetsUserInfos(string username, string password)
+
+        public IEnumerable<UserInfo> GetsUserInfos(string userEmail, string password)
         {
-            return _userInfoRepository.Query(p => p.UserName == username && p.Password == password).Include(x=>x.UserHomeLinks).Select();
+            return _userInfoRepository.Query(p => p.Email == userEmail && p.Password == password).Include(x => x.UserHomeLinks).Select();
 
         }
+
         public bool IsLoginIdUnique(string email)
         {
             return _userInfoRepository.Queryable().Any(p => p.Email == email);
         }
+
         public bool IsValidLogin(string email, string pass)
         {
             return _userInfoRepository.Query(p => p.Email == email && p.Password == pass).Select().Count() == 0 ? false : true;
         }
-        public IEnumerable<UserInfo> GetUserInfos(string email, string pass)
-        {
-            var tempCha = _channelRepository.Queryable().Include(x => x.ChannelStatuses).ToList();
 
-            var temp=_userInfoRepository.Queryable().Where(x => x.Email == email && x.Password == pass)
-                .Include(x => x.UserHomeLinks.Select(y => y.Home.SmartRouterInfoes))
-                .Include(x => x.UserHomeLinks.Select(y => y.Home))
-                .Include(x => x.UserRoomLinks.Select(y => y.Room.SmartDevices.Select(p => p.DeviceStatus))).ToList();
 
-            return temp;
-        }
         public UserInfoEntity Add(UserInfoEntity entity)
         {
             Mapper.CreateMap<UserInfoEntity, Model.Models.UserInfo>()
@@ -63,14 +58,16 @@ namespace SmartHome.Service
             .ForMember(dest => dest.AuditField, opt => opt.UseValue(new AuditFields()))
             .ForMember(dest => dest.ObjectState, opt => opt.UseValue(ObjectState.Added));//state                                                                                         
             Model.Models.UserInfo model = Mapper.Map<Entity.UserInfoEntity, Model.Models.UserInfo>(entity);
-
             _userInfoRepository.Insert(model);
             return entity;
-            
+
         }
+
+
         public IEnumerable<WebPagesRole> GetsWebPagesRoles()
         {
             return _webPagesRoleRepository.Query().Select();
         }
+
     }
 }
