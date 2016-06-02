1b116db314b50160906731bec69ef082e42ec996
sumon.kumar <sumon.kumar@aplombtechbd.com>
2016-06-02 09:42:28 +0600

    add pk

--- SmartHome.Model/Models/NextAssociatedDevice.cs
+++ SmartHome.Model/Models/NextAssociatedDevice.cs
@@ -1,6 +1,8 @@
 ﻿using Repository.Pattern.Ef6;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +11,10 @@ namespace SmartHome.Model.Models
 {
     public class NextAssociatedDevice:Entity
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int NextAssociatedDeviceId { get; set; }
+        
         public int NextDeviceId { get; set; }
     }
 }
