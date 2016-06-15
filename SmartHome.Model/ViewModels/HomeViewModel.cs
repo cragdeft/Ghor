fc6d510de7a46df3c38dbc81229856bd13bdd1b6
md.shamim <imsiddiquee@gmail.com>
2016-06-15 12:42:30 +0600

    add constructor for list property initialization

--- SmartHome.Model/ViewModels/HomeViewModel.cs
+++ SmartHome.Model/ViewModels/HomeViewModel.cs
@@ -9,15 +9,30 @@ namespace SmartHome.Model.ViewModels
 {
     public class HomeViewModel
     {
+        public HomeViewModel()
+        {
+
+            Homes = new List<Home>();
+            Users = new List<UserInfo>();
+            UserHomeLinks = new List<UserHomeLink>();
+            Routers = new List<RouterInfo>();
+            Rooms = new List<Room>();
+            UserRoomLinks = new List<UserRoomLink>();
+            SmartDevices = new List<SmartDevice>();
+            NextAssociatedDevice = new List<NextAssociatedDevice>();
+            Channels = new List<Channel>();
+            RgbwStatuses = new List<RgbwStatus>();
+        }
+
         public IList<Home> Homes { get; set; }
         public IList<UserInfo> Users { get; set; }
-        public IList<UserHomeLink> UserHomeLinks { get; set; } 
+        public IList<UserHomeLink> UserHomeLinks { get; set; }
         public IList<RouterInfo> Routers { get; set; }
         public IList<Room> Rooms { get; set; }
-        public IList<UserRoomLink> UserRoomLinks { get; set; } 
+        public IList<UserRoomLink> UserRoomLinks { get; set; }
         public IList<SmartDevice> SmartDevices { get; set; }
         public IList<NextAssociatedDevice> NextAssociatedDevice { get; set; }
         public IList<Channel> Channels { get; set; }
-        public IList<RgbwStatus> RgbwStatuses { get; set; }     
+        public IList<RgbwStatus> RgbwStatuses { get; set; }
     }
 }
