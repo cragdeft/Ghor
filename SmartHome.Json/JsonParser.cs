8f5e8a92ab956917d3ace73f548767acc6c8a1fc
md.shamim <imsiddiquee@gmail.com>
2016-08-28 17:45:28 +0600

    update json parser

--- SmartHome.Json/JsonParser.cs
+++ SmartHome.Json/JsonParser.cs
@@ -8,6 +8,7 @@ using Repository.Pattern.DataContext;
 using Repository.Pattern.Ef6;
 using Repository.Pattern.UnitOfWork;
 using SmartHome.Entity;
+using SmartHome.Model.Enums;
 using SmartHome.Model.ModelDataContext;
 using SmartHome.Service;
 using SmartHome.Service.Interfaces;
@@ -17,19 +18,26 @@ namespace SmartHome.Json
     public class JsonParser
     {
         public HomeJsonEntity _homeJsonEntity { get; private set; }
+        public string _homeJsonMessage { get; private set; }
         private IUnitOfWorkAsync _unitOfWorkAsync;
         private IHomeJsonParserService _homeJsonParserService;
-        public JsonParser(string jsonString)
+        private MessageReceivedFrom _receivedFrom;
+
+        public JsonParser(string jsonString, MessageReceivedFrom receivedFrom)
         {
+            _receivedFrom = receivedFrom;
+            _homeJsonMessage = jsonString;
             _homeJsonEntity = JsonDesrialized<HomeJsonEntity>(jsonString);
             InitializeParameters();
         }
+
+        
 
         private void InitializeParameters()
         {
             IDataContextAsync context = new SmartHomeDataContext();
             _unitOfWorkAsync = new UnitOfWork(context);
-            _homeJsonParserService = new HomeJsonParserService(_unitOfWorkAsync,_homeJsonEntity);
+            _homeJsonParserService = new HomeJsonParserService(_unitOfWorkAsync,_homeJsonEntity, _homeJsonMessage,_receivedFrom);
         }
 
         public void Save()
