8bca64a7e008289dbd8b855aa3bf8f6baf8a2267
md.shamim <imsiddiquee@gmail.com>
2016-09-18 17:02:02 +0600

    add new interface IHomeUpdateJsonParserService

---
+++ SmartHome.Service/IHomeUpdateJsonParserService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHome.Service
+{ 
+    public interface IHomeUpdateJsonParserService
+    {
+        bool UpdateJsonData();
+
+    }
+}
