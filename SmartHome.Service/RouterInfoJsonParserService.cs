3c3a7e232c593463f942adb717a96d4abaae40d7
md.shamim <imsiddiquee@gmail.com>
2016-09-19 10:54:52 +0600

    add new service NewRouter

---
+++ SmartHome.Service/RouterInfoJsonParserService.cs
@@ -0,0 +1,99 @@
+﻿using AutoMapper;
+using Repository.Pattern.Infrastructure;
+using Repository.Pattern.Repositories;
+using Repository.Pattern.UnitOfWork;
+using SmartHome.Entity;
+using SmartHome.Model.Enums;
+using SmartHome.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHome.Service
+{
+    public class RouterInfoJsonParserService : IHomeJsonParserService
+    {
+        #region PrivateProperty
+        private readonly IUnitOfWorkAsync _unitOfWorkAsync;
+        private readonly IRepositoryAsync<Home> _homeRepository;
+        private readonly IRepositoryAsync<RouterInfo> _routerInfoRepository;
+        private readonly IRepositoryAsync<UserHomeLink> _userHomeRepository;
+
+        public HomeJsonEntity _homeJsonEntity { get; private set; }
+        public string _homeJsonMessage { get; private set; }
+        public MessageReceivedFrom _receivedFrom { get; private set; }
+        public MessageLog _messageLog { get; private set; }
+
+        #endregion
+        public RouterInfoJsonParserService(IUnitOfWorkAsync unitOfWorkAsync, HomeJsonEntity homeJsonEntity, string homeJsonMessage, MessageReceivedFrom receivedFrom)
+        {
+            _unitOfWorkAsync = unitOfWorkAsync;
+            _routerInfoRepository = _unitOfWorkAsync.RepositoryAsync<RouterInfo>();
+            _userHomeRepository = _unitOfWorkAsync.RepositoryAsync<UserHomeLink>();
+
+            _homeJsonEntity = homeJsonEntity;
+            _homeJsonMessage = homeJsonMessage;
+            _receivedFrom = receivedFrom;
+            _messageLog = new MessageLog();
+        }
+        public bool SaveJsonData()
+        {
+            MessageLog messageLog = new CommonService(_unitOfWorkAsync).SaveMessageLog(_homeJsonMessage, _receivedFrom);
+
+            _unitOfWorkAsync.BeginTransaction();
+
+            try
+            {
+                SaveNewSmartRouterInfo();
+                var changes = _unitOfWorkAsync.SaveChanges();
+                _unitOfWorkAsync.Commit();
+            }
+            catch (Exception ex)
+            {
+                _unitOfWorkAsync.Rollback();
+                return false;
+            }
+
+            new CommonService(_unitOfWorkAsync).UpdateMessageLog(messageLog, _homeJsonEntity.Home[0].PassPhrase);
+
+            return true;
+        }
+        private void SaveNewSmartRouterInfo()
+        {
+            string passPhrase = _homeJsonEntity.Home.FirstOrDefault().PassPhrase;
+            string deviceHash = _homeJsonEntity.Device.FirstOrDefault().DeviceHash;
+            string userEmail = _homeJsonEntity.UserInfo.FirstOrDefault().Email;
+
+            Home home = null;
+            UserInfo userInfo = null;
+
+            home = new CommonService(_unitOfWorkAsync).GetHome(passPhrase);
+            userInfo = new CommonService(_unitOfWorkAsync).GetUserByEmail(userEmail);
+
+            if (home != null && userInfo != null)
+            {
+                InsertRouter(_homeJsonEntity.RouterInfo[0], home);
+                UpdateUserHomeLink(userInfo);
+            }
+
+        }
+        private void UpdateUserHomeLink(UserInfo userInfo)
+        {
+            var entity = _userHomeRepository.Queryable().Where(p => p.UserInfo.UserInfoId == userInfo.UserInfoId).FirstOrDefault();
+            entity.IsAdmin = true;
+            entity.ObjectState = ObjectState.Modified;
+            _userHomeRepository.Update(entity);
+        }
+        private void InsertRouter(RouterInfoEntity router, Home home)
+        {
+            var entity = Mapper.Map<RouterInfoEntity, RouterInfo>(router);
+            entity.IsSynced = Convert.ToBoolean(router.IsSynced);
+            entity.Parent = home;
+            entity.AuditField = new AuditFields("admin", DateTime.Now, "admin", DateTime.Now);
+            entity.ObjectState = ObjectState.Added;
+            _routerInfoRepository.Insert(entity);
+        }
+    }
+}
