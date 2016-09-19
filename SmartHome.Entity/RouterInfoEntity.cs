fca8a84e62f4fb050c97a14bfdb33036f61224da
md.shamim <imsiddiquee@gmail.com>
2016-09-19 17:54:24 +0600

    add new properties

--- SmartHome.Entity/RouterInfoEntity.cs
+++ SmartHome.Entity/RouterInfoEntity.cs
@@ -12,18 +12,33 @@ namespace SmartHome.Entity
         #region Primitive Properties 
         [JsonProperty("Id")]
         public int AppsRouterInfoId { get; set; }
-        [JsonProperty("Home")]
-        public int AppsHomeId { get; set; }
+        [JsonProperty("DeviceId")]
+        public int AppsBleId { get; set; }
         [JsonProperty("LocalBrokerIp")]
         public string LocalBrokerIp { get; set; }
         [JsonProperty("LocalBrokerPort")]
         public string LocalBrokerPort { get; set; }
         public string LocalBrokerUsername { get; set; }
         public string LocalBrokerPassword { get; set; }
-        [JsonProperty("MacAddress")]
-        public string MacAddress { get; set; }
+        [JsonProperty("LocalBrokerExternalIp")]
+        public string LocalBrokerExternalIp { get; set; }
+        [JsonProperty("LocalBrokerExternalPort")]
+        public string LocalBrokerExternalPort { get; set; }
+        public int IsExternal { get; set; }
         public string Ssid { get; set; }
         public string SsidPassword { get; set; }
+        [JsonProperty("MacAddress")]
+        public string MacAddress { get; set; }
+        [JsonProperty("HttpdUsername")]
+        public string HttpdUsername { get; set; }
+        [JsonProperty("HttpdPassword")]
+        public string HttpdPassword { get; set; }
+        [JsonProperty("HttpdIp")]
+        public string HttpdIp { get; set; }
+        [JsonProperty("HttpdPort")]
+        public string HttpdPort { get; set; }
+        [JsonProperty("Home")]
+        public int AppsHomeId { get; set; }
         public int IsSynced { get; set; }
         public virtual HomeEntity Parent { get; set; }
         #endregion
