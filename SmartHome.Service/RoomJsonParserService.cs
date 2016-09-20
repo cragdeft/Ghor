a1439f273d76c7b2e85b2fc78c5ff89a08b70603
md.shamim <imsiddiquee@gmail.com>
2016-09-20 15:23:40 +0600

    update RoomNewEntryJsonParserService

--- SmartHome.Service/RoomJsonParserService.cs
+++ SmartHome.Service/RoomJsonParserService.cs
@@ -18,9 +18,6 @@ namespace SmartHome.Service
     {
         #region PrivateProperty
         private readonly IUnitOfWorkAsync _unitOfWorkAsync;
-        private readonly IRepositoryAsync<Room> _roomRepository;
-        private readonly IRepositoryAsync<UserRoomLink> _userRoomLinkRepository;
-
         public HomeJsonEntity _homeJsonEntity { get; private set; }
         public string _homeJsonMessage { get; private set; }
         public MessageReceivedFrom _receivedFrom { get; private set; }
@@ -29,8 +26,6 @@ namespace SmartHome.Service
         public RoomJsonParserService(IUnitOfWorkAsync unitOfWorkAsync, HomeJsonEntity homeJsonEntity, string homeJsonMessage, MessageReceivedFrom receivedFrom)
         {
             _unitOfWorkAsync = unitOfWorkAsync;
-            _roomRepository = _unitOfWorkAsync.RepositoryAsync<Room>();
-            _userRoomLinkRepository = _unitOfWorkAsync.RepositoryAsync<UserRoomLink>();
 
             _homeJsonEntity = homeJsonEntity;
             _homeJsonMessage = homeJsonMessage;
@@ -39,80 +34,31 @@ namespace SmartHome.Service
 
         public bool SaveJsonData()
         {
-            MessageLog messageLog = new CommonService(_unitOfWorkAsync).SaveMessageLog(_homeJsonMessage, _receivedFrom);
-
-            _unitOfWorkAsync.BeginTransaction();
-            SetMapper();
+            IHomeJsonParserService service = null;
+            bool isSuccess = false;
             try
             {
-                SaveNewRoom();
-                var changes = _unitOfWorkAsync.SaveChanges();
-                _unitOfWorkAsync.Commit();
+                string passPhrase = _homeJsonEntity.Home.FirstOrDefault().PassPhrase;
+
+                RoomEntity roomEntity = _homeJsonEntity.Room.FirstOrDefault();
+                Room dbRoom = new CommonService(_unitOfWorkAsync).GetRoomByPassPhaseAndAppsRoomId(passPhrase, roomEntity.AppsRoomId);
+
+                if (dbRoom != null)
+                {
+                    service = new RoomUpdateJsonParserService(_unitOfWorkAsync, _homeJsonEntity, _homeJsonMessage, MessageReceivedFrom.UpdateRoom);
+                }
+                else
+                {
+                    service = new RoomNewEntryJsonParserService(_unitOfWorkAsync, _homeJsonEntity, _homeJsonMessage, MessageReceivedFrom.NewRoom);
+                }
+                isSuccess = service.SaveJsonData();
+
             }
             catch (Exception ex)
             {
-                _unitOfWorkAsync.Rollback();
                 return false;
             }
-
-            new CommonService(_unitOfWorkAsync).UpdateMessageLog(messageLog, _homeJsonEntity.Home[0].PassPhrase);
-
-
-            return true;
-        }
-        private void SetMapper()
-        {
-            Mapper.CreateMap<RoomEntity, Room>();
-        }
-        private void SaveNewRoom()
-        {
-            string passPhrase = _homeJsonEntity.Home.FirstOrDefault().PassPhrase;
-            Home home = null;
-
-            home = new CommonService(_unitOfWorkAsync).GetHome(passPhrase);
-            if (home != null)
-            {
-                UserInfo userInfo = new CommonService(_unitOfWorkAsync).GetUser(_homeJsonEntity.UserInfo[0].Email);
-                home = SaveNewRoom(home, userInfo);
-            }
-        }
-        private Home SaveNewRoom(Home home, UserInfo userInfo)
-        {
-            RoomEntity room = _homeJsonEntity.Room[0];
-
-            var entity = Mapper.Map<RoomEntity, Room>(room);
-            entity.IsActive = Convert.ToBoolean(room.IsActive);
-            entity.IsSynced = Convert.ToBoolean(room.IsSynced);
-            entity.Home = home;
-            entity.AuditField = new AuditFields("admin", DateTime.Now, "admin", DateTime.Now);
-            entity.ObjectState = ObjectState.Added;
-            _roomRepository.Insert(entity);
-
-            SaveRoomUser(entity, userInfo);
-
-            return home;
-        }
-        private void SaveRoomUser(Room entity, UserInfo userInfo)
-        {
-            var roomLinkList = _homeJsonEntity.UserRoomLink.FindAll(x => x.AppsRoomId == entity.AppsRoomId);
-
-            foreach (var userRoomLinkEntity in roomLinkList)
-            {
-                UserRoomLink userRoom = new UserRoomLink();
-                userRoom.UserInfo = userInfo;
-
-                FillSaveRoomUser(entity, userRoomLinkEntity, userRoom);
-            }
-        }
-        private void FillSaveRoomUser(Room entity, UserRoomLinkEntity userRoomLinkEntity, UserRoomLink userRoom)
-        {
-            userRoom.Room = entity;
-            userRoom.IsSynced = Convert.ToBoolean(userRoomLinkEntity.IsSynced);
-            userRoom.AppsUserRoomLinkId = userRoomLinkEntity.AppsUserRoomLinkId;
-            userRoom.AppsRoomId = userRoomLinkEntity.AppsRoomId;
-            userRoom.AppsUserId = userRoomLinkEntity.AppsUserId;
-            userRoom.ObjectState = ObjectState.Added;
-            _userRoomLinkRepository.Insert(userRoom);
+            return isSuccess;
         }
     }
 }
---
+++ SmartHome.Service/RoomNewEntryJsonParserService.cs
@@ -0,0 +1,103 @@
+﻿using AutoMapper;
+using Repository.Pattern.Infrastructure;
+using Repository.Pattern.Repositories;
+using Repository.Pattern.UnitOfWork;
+using SmartHome.Entity;
+using SmartHome.Model.Enums;
+using SmartHome.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHome.Service
+{
+    public class RoomNewEntryJsonParserService : IHomeJsonParserService
+    {
+        #region PrivateProperty
+        private readonly IUnitOfWorkAsync _unitOfWorkAsync;
+        private readonly IRepositoryAsync<Room> _roomRepository;
+
+        public HomeJsonEntity _homeJsonEntity { get; private set; }
+        public string _homeJsonMessage { get; private set; }
+        public MessageReceivedFrom _receivedFrom { get; private set; }
+
+        #endregion
+        public RoomNewEntryJsonParserService(IUnitOfWorkAsync unitOfWorkAsync, HomeJsonEntity homeJsonEntity, string homeJsonMessage, MessageReceivedFrom receivedFrom)
+        {
+            _unitOfWorkAsync = unitOfWorkAsync;
+            _roomRepository = _unitOfWorkAsync.RepositoryAsync<Room>();
+
+            _homeJsonEntity = homeJsonEntity;
+            _homeJsonMessage = homeJsonMessage;
+            _receivedFrom = receivedFrom;
+        }
+
+        public bool SaveJsonData()
+        {
+            bool isSuccess = false;
+            MessageLog messageLog = new CommonService(_unitOfWorkAsync).SaveMessageLog(_homeJsonMessage, _receivedFrom);
+
+            _unitOfWorkAsync.BeginTransaction();
+            SetMapper();
+            try
+            {
+                isSuccess = SaveNewRoom();
+                var changes = _unitOfWorkAsync.SaveChanges();
+                _unitOfWorkAsync.Commit();
+            }
+            catch (Exception ex)
+            {
+                _unitOfWorkAsync.Rollback();
+                return false;
+            }
+
+            new CommonService(_unitOfWorkAsync).UpdateMessageLog(messageLog, _homeJsonEntity.Home[0].PassPhrase);
+
+
+            return isSuccess;
+        }
+        private void SetMapper()
+        {
+            Mapper.CreateMap<RoomEntity, Room>();
+        }
+        private bool SaveNewRoom()
+        {
+            string passPhrase = _homeJsonEntity.Home.FirstOrDefault().PassPhrase;
+
+            Home home = null;
+            Room room = null;
+            UserInfo userInfo = null;
+
+            bool isComplete = false;
+
+            home = new CommonService(_unitOfWorkAsync).GetHome(passPhrase);
+            userInfo = new CommonService(_unitOfWorkAsync).GetUser(_homeJsonEntity.UserInfo[0].Email);
+            if (home != null && userInfo != null)
+            {
+                room = SaveNewRoom(home);
+
+                IHomeJsonParserService service = new RoomUserNewEntryJsonParserService(_unitOfWorkAsync, _homeJsonEntity, room);
+                isComplete = service.SaveJsonData();
+            }
+            return isComplete;
+        }
+        private Room SaveNewRoom(Home home)
+        {
+            RoomEntity room = _homeJsonEntity.Room[0];
+
+            var entity = Mapper.Map<RoomEntity, Room>(room);
+            entity.IsActive = Convert.ToBoolean(room.IsActive);
+            entity.IsSynced = Convert.ToBoolean(room.IsSynced);
+            entity.Home = home;
+            entity.AuditField = new AuditFields("admin", DateTime.Now, "admin", DateTime.Now);
+            entity.ObjectState = ObjectState.Added;
+            _roomRepository.Insert(entity);
+
+            //SaveRoomUser(entity, userInfo);
+
+            return entity;
+        }
+    }
+}
