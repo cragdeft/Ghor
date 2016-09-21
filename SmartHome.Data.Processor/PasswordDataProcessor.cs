2072e0288994584b444098638bcfe2f914f7c99d
md.shamim <imsiddiquee@gmail.com>
2016-09-21 17:21:02 +0600

    add PasswordDataProcessor

---
+++ SmartHome.Data.Processor/PasswordDataProcessor.cs
@@ -0,0 +1,64 @@
+﻿using Repository.Pattern.DataContext;
+using Repository.Pattern.Ef6;
+using Repository.Pattern.UnitOfWork;
+using SmartHome.Model.Enums;
+using SmartHome.Model.ModelDataContext;
+using SmartHome.Model.Models;
+using SmartHome.Service;
+using SmartHome.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHome.Data.Processor
+{  
+    public class PasswordDataProcessor : BaseDataProcessor
+    {
+        public dynamic _userEntity { get; private set; }
+
+        public PasswordDataProcessor(string jsonString, MessageReceivedFrom receivedFrom)
+        {
+            _receivedFrom = receivedFrom;
+            _homeJsonMessage = jsonString;
+            _userEntity = JsonDesrialized<dynamic>(jsonString);
+        }
+
+
+        public bool ChangePassword()
+        {
+            MessageLog messageLog = null;
+            bool isSuccess = false;
+
+            if (_userEntity == null)
+                return false;
+
+            using (IDataContextAsync context = new SmartHomeDataContext())
+            using (IUnitOfWorkAsync unitOfWork = new UnitOfWork(context))
+            {
+
+                var transactionRunner = new UnitOfWorkTransactionRunner(unitOfWork);
+                try
+                {
+                    isSuccess = transactionRunner.RunSelectTransaction(() => new CommonService(unitOfWork).IsLoginIdUnique(Convert.ToString(_userEntity.Email)));
+
+                    if (isSuccess)
+                    {
+                        isSuccess = transactionRunner.RunTransaction(() => new CommonService(unitOfWork).PasswordUpdate(Convert.ToString(_userEntity.Email), Convert.ToString(_userEntity.Password)));
+                    }
+                    messageLog = transactionRunner.RunTransaction(() => new CommonService(unitOfWork).SaveMessageLog(_homeJsonMessage, _receivedFrom));
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+                finally
+                {
+                    transactionRunner.RunTransaction(() => new CommonService(unitOfWork).UpdateMessageLog(messageLog, string.Empty));
+                }
+            }
+            return isSuccess;
+        }
+    }
+}
