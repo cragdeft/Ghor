458178a93a2b79d7743a783b20a00df300fee7e4
md.shamim <imsiddiquee@gmail.com>
2016-09-21 17:21:39 +0600

    add RegisterUserDataProcessor

---
+++ SmartHome.Data.Processor/RegisterUserDataProcessor.cs
@@ -0,0 +1,71 @@
+﻿using Repository.Pattern.DataContext;
+using Repository.Pattern.Ef6;
+using Repository.Pattern.UnitOfWork;
+using SmartHome.Entity;
+using SmartHome.Model.Enums;
+using SmartHome.Model.ModelDataContext;
+using SmartHome.Model.Models;
+using SmartHome.Service;
+using SmartHome.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHome.Data.Processor
+{
+    public class RegisterUserDataProcessor : BaseDataProcessor
+    {
+        public LoginObjectEntity _userEntity { get; private set; }
+
+        public RegisterUserDataProcessor(string jsonString, MessageReceivedFrom receivedFrom)
+        {
+            _receivedFrom = receivedFrom;
+            _homeJsonMessage = jsonString;
+            _userEntity = JsonDesrialized<LoginObjectEntity>(jsonString);
+        }
+        public bool RegisterUser()
+        {
+            MessageLog messageLog = null;
+            bool isRegisterSucces = false;
+
+            if (_userEntity == null)
+                return false;
+
+            using (IDataContextAsync context = new SmartHomeDataContext())
+            using (IUnitOfWorkAsync unitOfWork = new UnitOfWork(context))
+            {
+
+                var transactionRunner = new UnitOfWorkTransactionRunner(unitOfWork);
+                try
+                {
+                    messageLog = transactionRunner.RunTransaction(() => new CommonService(unitOfWork).SaveMessageLog(_homeJsonMessage, _receivedFrom));
+
+                    bool isUserExist = transactionRunner.RunSelectTransaction(() => new CommonService(unitOfWork).IsLoginIdUnique(_userEntity.UserInfo.FirstOrDefault().Email));
+
+                    if (isUserExist)
+                    {
+                        isRegisterSucces = false;
+                    }
+                    else
+                    {
+                        transactionRunner.RunTransaction(() => new UserInfoService(unitOfWork).Add(_userEntity.UserInfo.FirstOrDefault()));
+                        isRegisterSucces = true;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+                finally
+                {
+                    transactionRunner.RunTransaction(() => new CommonService(unitOfWork).UpdateMessageLog(messageLog, string.Empty));
+                }
+            }
+            return isRegisterSucces;
+        }
+    }
+
+
+}
