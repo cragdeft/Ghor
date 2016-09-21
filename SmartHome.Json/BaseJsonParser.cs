3905447505639210899cbc2bf0986acc21710322
md.shamim <imsiddiquee@gmail.com>
2016-09-21 11:17:13 +0600

    add base json processor

---
+++ SmartHome.Json/BaseJsonParser.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using Repository.Pattern.UnitOfWork;
+using SmartHome.Entity;
+using SmartHome.Model.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Repository.Pattern.DataContext;
+namespace SmartHome.Json
+{
+    public class BaseJsonParser
+    {
+        public HomeJsonEntity _homeJsonEntity { get; set; }
+        public string _homeJsonMessage { get; set; }
+        public IUnitOfWorkAsync _unitOfWorkAsync { get; set; }
+        public MessageReceivedFrom _receivedFrom { get; set; }
+
+        public static T JsonDesrialized<T>(string jsonString)
+        {
+            return JsonConvert.DeserializeObject<T>(jsonString);
+        }
+    }
+}
