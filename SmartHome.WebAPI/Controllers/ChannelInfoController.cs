96ba7a035af942b9a810a7860b125d38a007baf9
md.shamim <imsiddiquee@gmail.com>
2016-09-25 13:06:43 +0600

    add new controller ChannelInfoController

---
+++ SmartHome.WebAPI/Controllers/ChannelInfoController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Net.Http;
+using System.Web.Http;
+using Newtonsoft.Json.Linq;
+using SmartHome.Entity;
+using SmartHome.Utility.EncryptionAndDecryption;
+using System.Net;
+using SmartHome.Data.Processor;
+using SmartHome.Model.Enums;
+using SmartHome.WebAPI.Utility;
+using SmartHome.Json;
+
+namespace SmartHome.WebAPI.Controllers
+{
+    public class ChannelInfoController : ApiController
+    {
+        [Route("api/NewChannel")]
+        [HttpPost]
+        public HttpResponseMessage NewChannel(JObject encryptedString)
+        {
+            HttpResponseMessage response;
+            PasswordRecoveryRootObjectEntity oRootObject = new PasswordRecoveryRootObjectEntity();
+            try
+            {
+                #region Initialization
+
+                oRootObject.data = new PasswordRecoveryObjectEntity();
+                string msg = string.Empty;
+
+                msg = SecurityManager.Decrypt(encryptedString["encryptedString"].ToString());
+                if (string.IsNullOrEmpty(msg))
+                {
+                    return response = Request.CreateResponse(HttpStatusCode.BadRequest, "Not have sufficient information to process.");
+                }
+
+                #endregion
+
+                ChannelJsonParser jsonManager = new ChannelJsonParser(msg, MessageReceivedFrom.NewChannel);
+                bool isSuccess = jsonManager.SaveNewChannel();
+
+                if (isSuccess)
+                {
+                    MessageResponseUtility.FillPasswordRecoveryInfos("", " New Channel Add Successfully.", HttpStatusCode.OK, oRootObject);
+                }
+                else
+                {
+                    MessageResponseUtility.FillPasswordRecoveryInfos("", " Can Not Add New Channel.", HttpStatusCode.BadRequest, oRootObject);
+                }
+
+                response = MessageResponseUtility.PrepareJsonResponse<PasswordRecoveryRootObjectEntity>(oRootObject);
+            }
+            catch (Exception ex)
+            {
+                MessageResponseUtility.FillPasswordRecoveryInfos(string.Empty, ex.ToString(), HttpStatusCode.BadRequest, oRootObject);
+                response = MessageResponseUtility.PrepareJsonResponse<PasswordRecoveryRootObjectEntity>(oRootObject);
+            }
+
+            return response;
+        }
+
+        [Route("api/DeleteChannel")]
+        [HttpPost]
+        public HttpResponseMessage DeleteChannel(JObject encryptedString)
+        {
+            HttpResponseMessage response;
+            PasswordRecoveryRootObjectEntity oRootObject = new PasswordRecoveryRootObjectEntity();
+            try
+            {
+                #region Initialization
+
+                oRootObject.data = new PasswordRecoveryObjectEntity();
+                string msg = string.Empty;
+
+                msg = SecurityManager.Decrypt(encryptedString["encryptedString"].ToString());
+                if (string.IsNullOrEmpty(msg))
+                {
+                    return response = Request.CreateResponse(HttpStatusCode.BadRequest, "Not have sufficient information to process.");
+                }
+
+                #endregion
+
+                ChannelDeleteJsonParser jsonManager = new ChannelDeleteJsonParser(msg, MessageReceivedFrom.DeleteChannel);
+                bool isSuccess = jsonManager.DeleteChannel();
+
+                if (isSuccess)
+                {
+                    MessageResponseUtility.FillPasswordRecoveryInfos("", " Channel Delete Successfully.", HttpStatusCode.OK, oRootObject);
+                }
+                else
+                {
+                    MessageResponseUtility.FillPasswordRecoveryInfos("", " Can Not Delete New Channel.", HttpStatusCode.BadRequest, oRootObject);
+                }
+
+                response = MessageResponseUtility.PrepareJsonResponse<PasswordRecoveryRootObjectEntity>(oRootObject);
+            }
+            catch (Exception ex)
+            {
+                MessageResponseUtility.FillPasswordRecoveryInfos(string.Empty, ex.ToString(), HttpStatusCode.BadRequest, oRootObject);
+                response = MessageResponseUtility.PrepareJsonResponse<PasswordRecoveryRootObjectEntity>(oRootObject);
+            }
+
+            return response;
+        }
+    }
+}
