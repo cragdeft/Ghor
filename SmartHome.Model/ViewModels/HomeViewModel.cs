1e9f582aed8d33b71bd235e50144ed07c2f5a57d
md.shamim <imsiddiquee@gmail.com>
2016-11-15 17:50:15 +0600

    add list CameraConfigInfoes

--- SmartHome.Model/ViewModels/HomeViewModel.cs
+++ SmartHome.Model/ViewModels/HomeViewModel.cs
@@ -7,35 +7,37 @@ using SmartHome.Model.Models;
 
 namespace SmartHome.Model.ViewModels
 {
-    public class HomeViewModel
+  public class HomeViewModel
+  {
+    public HomeViewModel()
     {
-        public HomeViewModel()
-        {
 
-            Homes = new List<Home>();
-            Users = new List<UserInfo>();
-            UserHomeLinks = new List<UserHomeLink>();
-            Routers = new List<RouterInfo>();
-            WebBrokerInfoes = new List<WebBrokerInfo>();
-            Rooms = new List<Room>();
-            UserRoomLinks = new List<UserRoomLink>();
-            SmartDevices = new List<SmartDevice>();
-            NextAssociatedDevice = new List<NextAssociatedDevice>();
-            Channels = new List<Channel>();
-            RgbwStatuses = new List<RgbwStatus>();
-        }
-
-        public IList<Home> Homes { get; set; }
-        public IList<UserInfo> Users { get; set; }
-        public IList<UserHomeLink> UserHomeLinks { get; set; }
-        public IList<RouterInfo> Routers { get; set; }
-        public IList<WebBrokerInfo> WebBrokerInfoes { get; set; }
-
-        public IList<Room> Rooms { get; set; }
-        public IList<UserRoomLink> UserRoomLinks { get; set; }
-        public IList<SmartDevice> SmartDevices { get; set; }
-        public IList<NextAssociatedDevice> NextAssociatedDevice { get; set; }
-        public IList<Channel> Channels { get; set; }
-        public IList<RgbwStatus> RgbwStatuses { get; set; }
+      Homes = new List<Home>();
+      Users = new List<UserInfo>();
+      UserHomeLinks = new List<UserHomeLink>();
+      Routers = new List<RouterInfo>();
+      WebBrokerInfoes = new List<WebBrokerInfo>();
+      Rooms = new List<Room>();
+      UserRoomLinks = new List<UserRoomLink>();
+      SmartDevices = new List<SmartDevice>();
+      NextAssociatedDevice = new List<NextAssociatedDevice>();
+      Channels = new List<Channel>();
+      RgbwStatuses = new List<RgbwStatus>();
     }
+
+    public IList<Home> Homes { get; set; }
+    public IList<UserInfo> Users { get; set; }
+    public IList<UserHomeLink> UserHomeLinks { get; set; }
+    public IList<RouterInfo> Routers { get; set; }
+    public IList<WebBrokerInfo> WebBrokerInfoes { get; set; }
+
+    public IList<CameraConfigInfo> CameraConfigInfoes { get; set; }
+
+    public IList<Room> Rooms { get; set; }
+    public IList<UserRoomLink> UserRoomLinks { get; set; }
+    public IList<SmartDevice> SmartDevices { get; set; }
+    public IList<NextAssociatedDevice> NextAssociatedDevice { get; set; }
+    public IList<Channel> Channels { get; set; }
+    public IList<RgbwStatus> RgbwStatuses { get; set; }
+  }
 }
