815163504b6faa0da16206e5ec299adf797f9d7f
md.shamim <imsiddiquee@gmail.com>
2016-11-15 17:49:42 +0600

    add camera config as child

--- SmartHome.Model/Models/Home.cs
+++ SmartHome.Model/Models/Home.cs
@@ -10,53 +10,54 @@ using System.Threading.Tasks;
 
 namespace SmartHome.Model.Models
 {
-    public class Home : Entity
+  public class Home : Entity
+  {
+    public Home()
     {
-        public Home()
-        {
-            MeshMode = MeshModeType.BLE;
-        }
-        #region Primitive Properties
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public long HomeId { get; set; }
-
-        public int AppsHomeId { get; set; }
-
-        public string Name { get; set; }
-        public string Address1 { get; set; }
-
-        public string Address2 { get; set; }
-        public string Block { get; set; }
-        public string City { get; set; }
-
-        public string ZipCode { get; set; }
-
-        public string Country { get; set; }
-
-        public string TimeZone { get; set; }
-        public bool IsActive { get; set; }
-        public bool IsDefault { get; set; }
-        public MeshModeType MeshMode { get; set; }
-        public string Phone { get; set; }
-        public string PassPhrase { get; set; }
-        public string Zone { get; set; }
-        public bool IsInternet { get; set; }
-
-        public bool IsSynced { get; set; }
-
-
-        #endregion
-
-        #region Complex Properties
-        public AuditFields AuditField { get; set; }
-
-        #endregion
-
-        #region Navigation Properties        
-        public virtual ICollection<Room> Rooms { get; set; }
-        public virtual ICollection<RouterInfo> SmartRouterInfos { get; set; }
-        public virtual ICollection<WebBrokerInfo> WebBrokerInfoes { get; set; }
-        #endregion
+      MeshMode = MeshModeType.BLE;
     }
+    #region Primitive Properties
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public long HomeId { get; set; }
+
+    public int AppsHomeId { get; set; }
+
+    public string Name { get; set; }
+    public string Address1 { get; set; }
+
+    public string Address2 { get; set; }
+    public string Block { get; set; }
+    public string City { get; set; }
+
+    public string ZipCode { get; set; }
+
+    public string Country { get; set; }
+
+    public string TimeZone { get; set; }
+    public bool IsActive { get; set; }
+    public bool IsDefault { get; set; }
+    public MeshModeType MeshMode { get; set; }
+    public string Phone { get; set; }
+    public string PassPhrase { get; set; }
+    public string Zone { get; set; }
+    public bool IsInternet { get; set; }
+
+    public bool IsSynced { get; set; }
+
+
+    #endregion
+
+    #region Complex Properties
+    public AuditFields AuditField { get; set; }
+
+    #endregion
+
+    #region Navigation Properties        
+    public virtual ICollection<Room> Rooms { get; set; }
+    public virtual ICollection<RouterInfo> SmartRouterInfos { get; set; }
+    public virtual ICollection<WebBrokerInfo> WebBrokerInfoes { get; set; }
+    public virtual ICollection<CameraConfigInfo> CameraConfigInfoes { get; set; }
+    #endregion
+  }
 }
