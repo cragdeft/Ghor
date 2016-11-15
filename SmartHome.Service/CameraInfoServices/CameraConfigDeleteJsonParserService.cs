64ae31471e85ed78448bd49dfc5210e24efea57f
md.shamim <imsiddiquee@gmail.com>
2016-11-15 10:47:31 +0600

    add camera config services for add ,update,delete

---
+++ SmartHome.Service/CameraInfoServices/CameraConfigDeleteJsonParserService.cs
@@ -0,0 +1,17 @@
+﻿using SmartHome.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHome.Service.CameraInfoServices
+{
+  class CameraConfigDeleteJsonParserService : IHomeDeleteJsonParserService<CameraConfigInfo>
+  {
+    public CameraConfigInfo DeleteJsonData()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ SmartHome.Service/CameraInfoServices/CameraConfigNewEntryJsonParserService.cs
@@ -0,0 +1,17 @@
+﻿using SmartHome.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHome.Service.CameraInfoServices
+{
+  public class CameraConfigNewEntryJsonParserService : IHomeJsonParserService<CameraConfigInfo>
+  {
+    public CameraConfigInfo SaveJsonData()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
