55298c4da72b89ea6c500881ac75389915ac3faf
md.shamim <imsiddiquee@gmail.com>
2016-11-15 17:55:33 +0600

    camera config entity add and list on root object entity

--- SmartHome.Entity/RoomJsonEntity.cs
+++ SmartHome.Entity/RoomJsonEntity.cs
@@ -6,25 +6,26 @@ using System.Threading.Tasks;
 
 namespace SmartHome.Entity
 {
-    
-    public class HomeJsonEntity
-    {
-        public List<VersionDetailEntity> VersionDetails { get; set; }
-        public List<HomeEntity> Home { get; set; }
-        public List<ChannelEntity> Channel { get; set; }
-        public List<SmartDeviceEntity> Device { get; set; }
-        public List<UserRoomLinkEntity> UserRoomLink { get; set; }
-        public List<NextAssociatedDeviceEntity> NextAssociatedDeviceId { get; set; }
-        public List<RouterInfoEntity> RouterInfo { get; set; }
-        public List<WebBrokerInfoEntity> WebBrokerInfo { get; set; }
-        public List<ChannelStatusEntity> ChannelStatus { get; set; }
-        public List<RoomEntity> Room { get; set; }
-        public int DatabaseVersion { get; set; }
-        public List<UserHomeLinkEntity> UserHomeLink { get; set; }
-        public List<UserInfoEntity> UserInfo { get; set; }
-        public List<VersionEntity> Version { get; set; }
-        public List<DeviceStatusEntity> DeviceStatus { get; set; }
-        public List<RgbwStatusEntity> RgbwStatus { get; set; }
-        public string Configuration { get; set; }
-    }
+
+  public class HomeJsonEntity
+  {
+    public List<VersionDetailEntity> VersionDetails { get; set; }
+    public List<HomeEntity> Home { get; set; }
+    public List<ChannelEntity> Channel { get; set; }
+    public List<SmartDeviceEntity> Device { get; set; }
+    public List<UserRoomLinkEntity> UserRoomLink { get; set; }
+    public List<NextAssociatedDeviceEntity> NextAssociatedDeviceId { get; set; }
+    public List<RouterInfoEntity> RouterInfo { get; set; }
+    public List<WebBrokerInfoEntity> WebBrokerInfo { get; set; }
+    public List<CameraConfigInfoEntity> CameraConfigInfo { get; set; }
+    public List<ChannelStatusEntity> ChannelStatus { get; set; }
+    public List<RoomEntity> Room { get; set; }
+    public int DatabaseVersion { get; set; }
+    public List<UserHomeLinkEntity> UserHomeLink { get; set; }
+    public List<UserInfoEntity> UserInfo { get; set; }
+    public List<VersionEntity> Version { get; set; }
+    public List<DeviceStatusEntity> DeviceStatus { get; set; }
+    public List<RgbwStatusEntity> RgbwStatus { get; set; }
+    public string Configuration { get; set; }
+  }
 }
--- SmartHome.Entity/RootObjectEntity.cs
+++ SmartHome.Entity/RootObjectEntity.cs
@@ -7,104 +7,106 @@ using System.Threading.Tasks;
 
 namespace SmartHome.Entity
 {
-    public class RootObjectEntity
+  public class RootObjectEntity
+  {
+    //public IEnumerable<VersionEntity> Version { get; set; }
+    //public IEnumerable<VersionDetailEntity> VersionDetails { get; set; }
+    //public IEnumerable<DeviceEntity> Device { get; set; }
+    //public IEnumerable<DeviceStatusEntity> DeviceStatus { get; set; }
+    //public IEnumerable<ChannelEntity> Channel { get; set; }
+    //public int NextDeviceID { get; set; }
+
+    public IEnumerable<UserInfoEntity> UserInfo { get; set; }
+    public IEnumerable<UserHomeLinkEntity> UserHomeLink { get; set; }
+    public IEnumerable<UserRoomLinkEntity> UserRoomLink { get; set; }
+
+    public IEnumerable<VersionEntity> Version { get; set; }
+    public IEnumerable<VersionDetailEntity> VersionDetails { get; set; }
+    public IEnumerable<SmartDeviceEntity> Device { get; set; }
+
+    public IEnumerable<RgbwStatusEntity> RgbwStatus { get; set; }
+    public IEnumerable<DeviceStatusEntity> DeviceStatus { get; set; }
+    public IEnumerable<ChannelEntity> Channel { get; set; }
+    public IEnumerable<ChannelStatusEntity> ChannelStatus { get; set; }
+    public IEnumerable<NextAssociatedDeviceEntity> NextAssociatedDeviceId { get; set; }
+
+    public IEnumerable<HomeEntity> Home { get; set; }
+
+    public IEnumerable<RoomEntity> Room { get; set; }
+
+    public IEnumerable<RouterInfoEntity> RouterInfo { get; set; }
+
+  }
+
+
+  public class LoginRootObjectEntity
+  {
+    public LoginMessage MESSAGE { get; set; }
+    public LoginObjectEntity data { get; set; }
+
+  }
+
+
+  public class LoginMessage
+  {
+    public int HTTP_STATUS { get; set; }
+    public string HTTP_MESSAGE { get; set; }
+  }
+
+
+  public class LoginObjectEntity
+  {
+    public LoginObjectEntity()
     {
-        //public IEnumerable<VersionEntity> Version { get; set; }
-        //public IEnumerable<VersionDetailEntity> VersionDetails { get; set; }
-        //public IEnumerable<DeviceEntity> Device { get; set; }
-        //public IEnumerable<DeviceStatusEntity> DeviceStatus { get; set; }
-        //public IEnumerable<ChannelEntity> Channel { get; set; }
-        //public int NextDeviceID { get; set; }
+      UserInfo = new List<UserInfoEntity>();
+      UserHomeLink = new List<UserHomeLinkEntity>();
+      UserRoomLink = new List<UserRoomLinkEntity>();
+      Home = new List<HomeEntity>();
+      Room = new List<RoomEntity>();
+      Device = new List<SmartDeviceEntity>();
+      Channel = new List<ChannelEntity>();
+      ChannelStatus = new List<ChannelStatusEntity>();
+      RgbwStatus = new List<RgbwStatusEntity>();
+      RouterInfo = new List<RouterInfoEntity>();
+      WebBrokerInfo = new List<WebBrokerInfoEntity>();
+      CameraConfigInfo = new List<CameraConfigInfoEntity>();
+      DeviceStatus = new List<DeviceStatusEntity>();
+      NextAssociatedDeviceId = new List<NextAssociatedDeviceEntity>();
+    }
+    public List<UserInfoEntity> UserInfo { get; set; }
+    public List<UserHomeLinkEntity> UserHomeLink { get; set; }
+    public List<UserRoomLinkEntity> UserRoomLink { get; set; }
+    public List<HomeEntity> Home { get; set; }
+    public List<RoomEntity> Room { get; set; }
+    public List<SmartDeviceEntity> Device { get; set; }
+    public List<DeviceStatusEntity> DeviceStatus { get; set; }
+    public List<ChannelEntity> Channel { get; set; }
+    public List<ChannelStatusEntity> ChannelStatus { get; set; }
+    public List<RgbwStatusEntity> RgbwStatus { get; set; }
+    public List<RouterInfoEntity> RouterInfo { get; set; }
+    public List<WebBrokerInfoEntity> WebBrokerInfo { get; set; }
+    public List<CameraConfigInfoEntity> CameraConfigInfo { get; set; }
+    public List<NextAssociatedDeviceEntity> NextAssociatedDeviceId { get; set; }
+  }
 
-        public IEnumerable<UserInfoEntity> UserInfo { get; set; }
-        public IEnumerable<UserHomeLinkEntity> UserHomeLink { get; set; }
-        public IEnumerable<UserRoomLinkEntity> UserRoomLink { get; set; }
+  public class PasswordRecoveryRootObjectEntity
+  {
+    public LoginMessage MESSAGE { get; set; }
+    public PasswordRecoveryObjectEntity data { get; set; }
+  }
 
-        public IEnumerable<VersionEntity> Version { get; set; }
-        public IEnumerable<VersionDetailEntity> VersionDetails { get; set; }
-        public IEnumerable<SmartDeviceEntity> Device { get; set; }
-
-        public IEnumerable<RgbwStatusEntity> RgbwStatus { get; set; }
-        public IEnumerable<DeviceStatusEntity> DeviceStatus { get; set; }
-        public IEnumerable<ChannelEntity> Channel { get; set; }
-        public IEnumerable<ChannelStatusEntity> ChannelStatus { get; set; }
-        public IEnumerable<NextAssociatedDeviceEntity> NextAssociatedDeviceId { get; set; }
-
-        public IEnumerable<HomeEntity> Home { get; set; }
-
-        public IEnumerable<RoomEntity> Room { get; set; }
-
-        public IEnumerable<RouterInfoEntity> RouterInfo { get; set; }
+  public class PasswordRecoveryObjectEntity
+  {
+    public PasswordRecoveryObjectEntity()
+    {
+      Password = string.Empty;
+      UserName = string.Empty;
 
     }
-
-
-    public class LoginRootObjectEntity
-    {
-        public LoginMessage MESSAGE { get; set; }
-        public LoginObjectEntity data { get; set; }
-
-    }
-
-
-    public class LoginMessage
-    {
-        public int HTTP_STATUS { get; set; }
-        public string HTTP_MESSAGE { get; set; }
-    }
-
-
-    public class LoginObjectEntity
-    {
-        public LoginObjectEntity()
-        {
-            UserInfo = new List<UserInfoEntity>();
-            UserHomeLink = new List<UserHomeLinkEntity>();
-            UserRoomLink = new List<UserRoomLinkEntity>();
-            Home = new List<HomeEntity>();
-            Room = new List<RoomEntity>();
-            Device = new List<SmartDeviceEntity>();
-            Channel = new List<ChannelEntity>();
-            ChannelStatus = new List<ChannelStatusEntity>();
-            RgbwStatus = new List<RgbwStatusEntity>();
-            RouterInfo = new List<RouterInfoEntity>();
-            WebBrokerInfo = new List<WebBrokerInfoEntity>();
-            DeviceStatus = new List<DeviceStatusEntity>();
-            NextAssociatedDeviceId = new List<NextAssociatedDeviceEntity>();
-        }
-        public List<UserInfoEntity> UserInfo { get; set; }
-        public List<UserHomeLinkEntity> UserHomeLink { get; set; }
-        public List<UserRoomLinkEntity> UserRoomLink { get; set; }
-        public List<HomeEntity> Home { get; set; }
-        public List<RoomEntity> Room { get; set; }
-        public List<SmartDeviceEntity> Device { get; set; }
-        public List<DeviceStatusEntity> DeviceStatus { get; set; }
-        public List<ChannelEntity> Channel { get; set; }
-        public List<ChannelStatusEntity> ChannelStatus { get; set; }
-        public List<RgbwStatusEntity> RgbwStatus { get; set; }
-        public List<RouterInfoEntity> RouterInfo { get; set; }
-        public List<WebBrokerInfoEntity> WebBrokerInfo { get; set; }
-        public List<NextAssociatedDeviceEntity> NextAssociatedDeviceId { get; set; }
-    }
-
-    public class PasswordRecoveryRootObjectEntity
-    {
-        public LoginMessage MESSAGE { get; set; }
-        public PasswordRecoveryObjectEntity data { get; set; }
-    }
-
-    public class PasswordRecoveryObjectEntity
-    {
-        public PasswordRecoveryObjectEntity()
-        {
-            Password = string.Empty;
-            UserName = string.Empty;
-
-        }
-        public string Password { get; set; }
-        public string UserName { get; set; }
+    public string Password { get; set; }
+    public string UserName { get; set; }
 
 
 
-    }
+  }
 }
