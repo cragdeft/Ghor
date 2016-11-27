e6a7429ace896e5fab265408968820923b30d52a
md.shamim <imsiddiquee@gmail.com>
2016-11-27 10:13:30 +0600

    parent home information add in UpdateSmartRouterInfo

--- SmartHome.Service/RouterInfoServices/RouterInfoUpdateJsonParserService.cs
+++ SmartHome.Service/RouterInfoServices/RouterInfoUpdateJsonParserService.cs
@@ -14,115 +14,115 @@ using System.Threading.Tasks;
 
 namespace SmartHome.Service
 {
-    public class RouterInfoUpdateJsonParserService : IHomeUpdateJsonParserService<RouterInfo>
+  public class RouterInfoUpdateJsonParserService : IHomeUpdateJsonParserService<RouterInfo>
+  {
+    #region PrivateProperty
+    private readonly IUnitOfWorkAsync _unitOfWorkAsync;
+    private readonly IRepositoryAsync<Home> _homeRepository;
+    private readonly IRepositoryAsync<RouterInfo> _routerInfoRepository;
+    private readonly IRepositoryAsync<WebBrokerInfo> _webBrokerInfoRepository;
+    private readonly IRepositoryAsync<UserHomeLink> _userHomeRepository;
+
+    public HomeJsonEntity _homeJsonEntity { get; private set; }
+    public string _homeJsonMessage { get; private set; }
+    public MessageReceivedFrom _receivedFrom { get; private set; }
+    public MessageLog _messageLog { get; private set; }
+
+    #endregion
+    public RouterInfoUpdateJsonParserService(IUnitOfWorkAsync unitOfWorkAsync, HomeJsonEntity homeJsonEntity, string homeJsonMessage, MessageReceivedFrom receivedFrom)
     {
-        #region PrivateProperty
-        private readonly IUnitOfWorkAsync _unitOfWorkAsync;
-        private readonly IRepositoryAsync<Home> _homeRepository;
-        private readonly IRepositoryAsync<RouterInfo> _routerInfoRepository;
-        private readonly IRepositoryAsync<WebBrokerInfo> _webBrokerInfoRepository;
-        private readonly IRepositoryAsync<UserHomeLink> _userHomeRepository;
+      _unitOfWorkAsync = unitOfWorkAsync;
+      _routerInfoRepository = _unitOfWorkAsync.RepositoryAsync<RouterInfo>();
+      _userHomeRepository = _unitOfWorkAsync.RepositoryAsync<UserHomeLink>();
+      _webBrokerInfoRepository = _unitOfWorkAsync.RepositoryAsync<WebBrokerInfo>();
 
-        public HomeJsonEntity _homeJsonEntity { get; private set; }
-        public string _homeJsonMessage { get; private set; }
-        public MessageReceivedFrom _receivedFrom { get; private set; }
-        public MessageLog _messageLog { get; private set; }
-
-        #endregion
-        public RouterInfoUpdateJsonParserService(IUnitOfWorkAsync unitOfWorkAsync, HomeJsonEntity homeJsonEntity, string homeJsonMessage, MessageReceivedFrom receivedFrom)
-        {
-            _unitOfWorkAsync = unitOfWorkAsync;
-            _routerInfoRepository = _unitOfWorkAsync.RepositoryAsync<RouterInfo>();
-            _userHomeRepository = _unitOfWorkAsync.RepositoryAsync<UserHomeLink>();
-            _webBrokerInfoRepository = _unitOfWorkAsync.RepositoryAsync<WebBrokerInfo>();
-
-            _homeJsonEntity = homeJsonEntity;
-            _homeJsonMessage = homeJsonMessage;
-            _receivedFrom = receivedFrom;
-            _messageLog = new MessageLog();
-        }
-        public RouterInfo UpdateJsonData()
-        {
-            RouterInfo routerInfo = null;
-            SetMapper();
-            try
-            {
-                routerInfo = UpdateSmartRouterInfo();
-            }
-            catch (Exception ex)
-            {
-                return null;
-            }
-            return routerInfo;
-        }
-        private RouterInfo UpdateSmartRouterInfo()
-        {
-            string passPhrase = _homeJsonEntity.Home.FirstOrDefault().PassPhrase;
-            string macAddress = _homeJsonEntity.RouterInfo.FirstOrDefault().MacAddress;
-            string userEmail = _homeJsonEntity.UserInfo.FirstOrDefault().Email;
-            RouterInfoEntity routerEntity = _homeJsonEntity.RouterInfo.FirstOrDefault();
-
-            Home home = null;
-            UserInfo userInfo = null;
-            RouterInfo routerInfo = null;
-
-            home = new CommonService(_unitOfWorkAsync).GetHome(passPhrase);
-            userInfo = new CommonService(_unitOfWorkAsync).GetUserByEmail(userEmail);
-            routerInfo = new CommonService(_unitOfWorkAsync).GetRouterInfoByMacAddress(macAddress);
-
-            if (routerInfo != null)
-            {
-                UpdateWebBrokerInfo(_homeJsonEntity.WebBrokerInfo[0], home);
-                UpdateUserHomeLink(userInfo);
-                return UpdateRouter(routerEntity, routerInfo);
-            }
-            return null;
-
-        }
-
-        private void UpdateWebBrokerInfo(WebBrokerInfoEntity webBrokerInfo, Home home)
-        {
-            WebBrokerInfo dbWebBrokerInfo = _webBrokerInfoRepository.Queryable().Where(p => p.Parent.HomeId == home.HomeId).FirstOrDefault();
-            if (dbWebBrokerInfo != null)
-            {
-                WebBrokerInfo entity = SmartHomeTranslater.MapWebBrokerInfoProperties(webBrokerInfo, dbWebBrokerInfo);
-
-                entity.AuditField = new AuditFields("admin", DateTime.Now, "admin", DateTime.Now);
-                entity.ObjectState = ObjectState.Modified;
-                _webBrokerInfoRepository.Update(entity);
-            }
-        }
-
-        private void UpdateUserHomeLink(UserInfo userInfo)
-        {
-            var entity = _userHomeRepository.Queryable().Where(p => p.UserInfo.UserInfoId == userInfo.UserInfoId).FirstOrDefault();
-            entity.IsAdmin = true;
-            entity.ObjectState = ObjectState.Modified;
-            _userHomeRepository.Update(entity);
-        }
-        public RouterInfo UpdateRouter(RouterInfoEntity routerEntity, RouterInfo router)
-        {
-            var entity = SmartHomeTranslater.MapRouterInfoProperties(routerEntity, router);
-            //entity.IsSynced = Convert.ToBoolean(routerEntity.IsSynced);
-            //entity.IsExternal = Convert.ToBoolean(routerEntity.IsExternal);
-            //entity.Parent = home;
-            entity.AuditField = new AuditFields("admin", DateTime.Now, "admin", DateTime.Now);
-            entity.ObjectState = ObjectState.Modified;
-            _routerInfoRepository.Update(entity);
-
-            return entity;
-        }
-
-        public void DeleteRouter(RouterInfo router)
-        {
-            router.ObjectState = ObjectState.Deleted;
-            _routerInfoRepository.Delete(router);
-        }
-
-        private void SetMapper()
-        {
-            Mapper.CreateMap<RouterInfoEntity, RouterInfo>();
-            Mapper.CreateMap<WebBrokerInfoEntity, WebBrokerInfo>();
-        }
+      _homeJsonEntity = homeJsonEntity;
+      _homeJsonMessage = homeJsonMessage;
+      _receivedFrom = receivedFrom;
+      _messageLog = new MessageLog();
     }
+    public RouterInfo UpdateJsonData()
+    {
+      RouterInfo routerInfo = null;
+      SetMapper();
+      try
+      {
+        routerInfo = UpdateSmartRouterInfo();
+      }
+      catch (Exception ex)
+      {
+        return null;
+      }
+      return routerInfo;
+    }
+    private RouterInfo UpdateSmartRouterInfo()
+    {
+      string passPhrase = _homeJsonEntity.Home.FirstOrDefault().PassPhrase;
+      string macAddress = _homeJsonEntity.RouterInfo.FirstOrDefault().MacAddress;
+      string userEmail = _homeJsonEntity.UserInfo.FirstOrDefault().Email;
+      RouterInfoEntity routerEntity = _homeJsonEntity.RouterInfo.FirstOrDefault();
+
+      Home home = null;
+      UserInfo userInfo = null;
+      RouterInfo routerInfo = null;
+
+      home = new CommonService(_unitOfWorkAsync).GetHome(passPhrase);
+      userInfo = new CommonService(_unitOfWorkAsync).GetUserByEmail(userEmail);
+      routerInfo = new CommonService(_unitOfWorkAsync).GetRouterInfoByMacAddressAndHomeId(macAddress, home.HomeId);
+
+      if (routerInfo != null)
+      {
+        UpdateWebBrokerInfo(_homeJsonEntity.WebBrokerInfo[0], home);
+        UpdateUserHomeLink(userInfo);
+        return UpdateRouter(routerEntity, routerInfo, home);
+      }
+      return null;
+
+    }
+
+    private void UpdateWebBrokerInfo(WebBrokerInfoEntity webBrokerInfo, Home home)
+    {
+      WebBrokerInfo dbWebBrokerInfo = _webBrokerInfoRepository.Queryable().Where(p => p.Parent.HomeId == home.HomeId).FirstOrDefault();
+      if (dbWebBrokerInfo != null)
+      {
+        WebBrokerInfo entity = SmartHomeTranslater.MapWebBrokerInfoProperties(webBrokerInfo, dbWebBrokerInfo);
+
+        entity.AuditField = new AuditFields("admin", DateTime.Now, "admin", DateTime.Now);
+        entity.ObjectState = ObjectState.Modified;
+        _webBrokerInfoRepository.Update(entity);
+      }
+    }
+
+    private void UpdateUserHomeLink(UserInfo userInfo)
+    {
+      var entity = _userHomeRepository.Queryable().Where(p => p.UserInfo.UserInfoId == userInfo.UserInfoId).FirstOrDefault();
+      entity.IsAdmin = true;
+      entity.ObjectState = ObjectState.Modified;
+      _userHomeRepository.Update(entity);
+    }
+    public RouterInfo UpdateRouter(RouterInfoEntity routerEntity, RouterInfo router, Home home)
+    {
+      var entity = SmartHomeTranslater.MapRouterInfoProperties(routerEntity, router);
+      //entity.IsSynced = Convert.ToBoolean(routerEntity.IsSynced);
+      //entity.IsExternal = Convert.ToBoolean(routerEntity.IsExternal);
+      //entity.Parent = home;
+      entity.AuditField = new AuditFields("admin", DateTime.Now, "admin", DateTime.Now);
+      entity.ObjectState = ObjectState.Modified;
+      _routerInfoRepository.Update(entity);
+
+      return entity;
+    }
+
+    public void DeleteRouter(RouterInfo router)
+    {
+      router.ObjectState = ObjectState.Deleted;
+      _routerInfoRepository.Delete(router);
+    }
+
+    private void SetMapper()
+    {
+      Mapper.CreateMap<RouterInfoEntity, RouterInfo>();
+      Mapper.CreateMap<WebBrokerInfoEntity, WebBrokerInfo>();
+    }
+  }
 }
