f476fd10d2e0fc51f8000985a30857b1f80324cd
md.shamim <imsiddiquee@gmail.com>
2016-11-27 10:19:03 +0600

    action filter add in web api project

--- SmartHome.WebAPI/Controllers/DeviceInfoController.cs
+++ SmartHome.WebAPI/Controllers/DeviceInfoController.cs
@@ -12,139 +12,139 @@ using SmartHome.Json;
 
 namespace SmartHome.WebAPI.Controllers
 {
-    public class DeviceInfoController : ApiController
+  public class DeviceInfoController : ApiController
+  {
+    [Route("api/NewDevice")]
+    [HttpPost]
+    public HttpResponseMessage NewDevice(JObject encryptedString)
     {
-        [Route("api/NewDevice")]
-        [HttpPost]
-        public HttpResponseMessage NewDevice(JObject encryptedString)
+      HttpResponseMessage response;
+      PasswordRecoveryRootObjectEntity oRootObject = new PasswordRecoveryRootObjectEntity();
+      try
+      {
+        #region Initialization
+
+        oRootObject.data = new PasswordRecoveryObjectEntity();
+        string msg = string.Empty;
+
+        msg = SecurityManager.Decrypt(encryptedString["encryptedString"].ToString());
+        //msg = "{\"Device\":[{\"Version\":0,\"Id\":2,\"Watt\":0,\"IsDeleted\":0,\"DeviceType\":1,\"DeviceName\":\"SMSW6G-00 1205326898\",\"DeviceId\":32773,\"DeviceHash\":1205326898,\"Room\":1}],\"Configuration\":\"NewDevice\",\"DeviceStatus\":[{\"StatusType\":53,\"StatusValue\":1,\"DeviceTableId\":2,\"Id\":1},{\"StatusType\":55,\"StatusValue\":0,\"DeviceTableId\":2,\"Id\":2},{\"StatusType\":5,\"StatusValue\":1,\"DeviceTableId\":2,\"Id\":3}],\"Home\":[{\"Block\":\"Block\",\"Country\":\"Country\",\"Address1\":\"address1\",\"Address2\":\"address2\",\"Id\":2,\"Name\":\"MyHome\",\"City\":\"City\",\"IsSynced\":1,\"Zone\":\"123\",\"Phone\":\"Phone\",\"TimeZone\":\"123\",\"IsDefault\":1,\"IsInternet\":1,\"IsActive\":1,\"ZipCode\":\"1215\",\"MeshMode\":0,\"PassPhrase\":\"B1E97D\"}]}";
+        if (string.IsNullOrEmpty(msg))
         {
-            HttpResponseMessage response;
-            PasswordRecoveryRootObjectEntity oRootObject = new PasswordRecoveryRootObjectEntity();
-            try
-            {
-                #region Initialization
-
-                oRootObject.data = new PasswordRecoveryObjectEntity();
-                string msg = string.Empty;
-
-                msg = SecurityManager.Decrypt(encryptedString["encryptedString"].ToString());
-                //msg = "{\"Device\":[{\"Version\":0,\"Id\":2,\"Watt\":0,\"IsDeleted\":0,\"DeviceType\":1,\"DeviceName\":\"SMSW6G-00 1205326898\",\"DeviceId\":32773,\"DeviceHash\":1205326898,\"Room\":1}],\"Configuration\":\"NewDevice\",\"DeviceStatus\":[{\"StatusType\":53,\"StatusValue\":1,\"DeviceTableId\":2,\"Id\":1},{\"StatusType\":55,\"StatusValue\":0,\"DeviceTableId\":2,\"Id\":2},{\"StatusType\":5,\"StatusValue\":1,\"DeviceTableId\":2,\"Id\":3}],\"Home\":[{\"Block\":\"Block\",\"Country\":\"Country\",\"Address1\":\"address1\",\"Address2\":\"address2\",\"Id\":2,\"Name\":\"MyHome\",\"City\":\"City\",\"IsSynced\":1,\"Zone\":\"123\",\"Phone\":\"Phone\",\"TimeZone\":\"123\",\"IsDefault\":1,\"IsInternet\":1,\"IsActive\":1,\"ZipCode\":\"1215\",\"MeshMode\":0,\"PassPhrase\":\"B1E97D\"}]}";
-                if (string.IsNullOrEmpty(msg))
-                {
-                    return response = Request.CreateResponse(HttpStatusCode.BadRequest, "Not have sufficient information to process.");
-                }
-
-                #endregion
-
-                DeviceJsonParser jsonManager = new DeviceJsonParser(msg, MessageReceivedFrom.NewDevice);
-                bool isSuccess = jsonManager.SaveNewDevice();
-
-                if (isSuccess)
-                {
-                    MessageResponseUtility.FillPasswordRecoveryInfos("", " New Device Add Successfully.", HttpStatusCode.OK, oRootObject);
-                }
-                else
-                {
-                    MessageResponseUtility.FillPasswordRecoveryInfos("", " Can Not Add New Device.", HttpStatusCode.BadRequest, oRootObject);
-                }
-
-                response = MessageResponseUtility.PrepareJsonResponse<PasswordRecoveryRootObjectEntity>(oRootObject);
-            }
-            catch (Exception ex)
-            {
-                MessageResponseUtility.FillPasswordRecoveryInfos(string.Empty, ex.ToString(), HttpStatusCode.BadRequest, oRootObject);
-                response = MessageResponseUtility.PrepareJsonResponse<PasswordRecoveryRootObjectEntity>(oRootObject);
-            }
-
-            return response;
-        }
-        [Route("api/DeleteDevice")]
-        [HttpPost]
-        public HttpResponseMessage DeleteDevice(JObject encryptedString)
-        {
-            HttpResponseMessage response;
-            PasswordRecoveryRootObjectEntity oRootObject = new PasswordRecoveryRootObjectEntity();
-            try
-            {
-                #region Initialization
-
-                oRootObject.data = new PasswordRecoveryObjectEntity();
-                string msg = string.Empty;
-
-                msg = SecurityManager.Decrypt(encryptedString["encryptedString"].ToString());
-                if (string.IsNullOrEmpty(msg))
-                {
-                    return response = Request.CreateResponse(HttpStatusCode.BadRequest, "Not have sufficient information to process.");
-                }
-
-                #endregion
-
-                DeviceDeleteJsonParser jsonManager = new DeviceDeleteJsonParser(msg, MessageReceivedFrom.DeleteDevice);
-                bool isSuccess = jsonManager.DeleteDevice();
-
-                if (isSuccess)
-                {
-                    MessageResponseUtility.FillPasswordRecoveryInfos("", " Device Delete Successfully.", HttpStatusCode.OK, oRootObject);
-                }
-                else
-                {
-                    MessageResponseUtility.FillPasswordRecoveryInfos("", " Can Not Delete Device.", HttpStatusCode.BadRequest, oRootObject);
-                }
-
-                response = MessageResponseUtility.PrepareJsonResponse<PasswordRecoveryRootObjectEntity>(oRootObject);
-            }
-            catch (Exception ex)
-            {
-                MessageResponseUtility.FillPasswordRecoveryInfos(string.Empty, ex.ToString(), HttpStatusCode.BadRequest, oRootObject);
-                response = MessageResponseUtility.PrepareJsonResponse<PasswordRecoveryRootObjectEntity>(oRootObject);
-            }
-
-            return response;
+          return response = Request.CreateResponse(HttpStatusCode.BadRequest, "Not have sufficient information to process.");
         }
 
-        [Route("api/NewRouter")]
-        [HttpPost]
-        public HttpResponseMessage NewRouter(JObject encryptedString)
+        #endregion
+
+        DeviceJsonParser jsonManager = new DeviceJsonParser(msg, MessageReceivedFrom.NewDevice);
+        bool isSuccess = jsonManager.SaveNewDevice();
+
+        if (isSuccess)
         {
-            HttpResponseMessage response;
-            PasswordRecoveryRootObjectEntity oRootObject = new PasswordRecoveryRootObjectEntity();
-            try
-            {
-                #region Initialization
-
-                oRootObject.data = new PasswordRecoveryObjectEntity();
-                string msg = string.Empty;
-
-                msg = SecurityManager.Decrypt(encryptedString["encryptedString"].ToString());
-                if (string.IsNullOrEmpty(msg))
-                {
-                    return response = Request.CreateResponse(HttpStatusCode.BadRequest, "Not have sufficient information to process.");
-                }
-
-                #endregion
-
-                RouterJsonParser jsonManager = new RouterJsonParser(msg, MessageReceivedFrom.NewRouter);
-                bool isSuccess = jsonManager.SaveNewRouter();
-
-                if (isSuccess)
-                {
-                    MessageResponseUtility.FillPasswordRecoveryInfos("", " New Router Add Successfully.", HttpStatusCode.OK, oRootObject);
-                }
-                else
-                {
-                    MessageResponseUtility.FillPasswordRecoveryInfos("", " Can Not Add New Router.", HttpStatusCode.BadRequest, oRootObject);
-                }
-
-                response = MessageResponseUtility.PrepareJsonResponse<PasswordRecoveryRootObjectEntity>(oRootObject);
-            }
-            catch (Exception ex)
-            {
-                MessageResponseUtility.FillPasswordRecoveryInfos(string.Empty, ex.ToString(), HttpStatusCode.BadRequest, oRootObject);
-                response = MessageResponseUtility.PrepareJsonResponse<PasswordRecoveryRootObjectEntity>(oRootObject);
-            }
-
-            return response;
+          MessageResponseUtility.FillPasswordRecoveryInfos("", " New Device Add Successfully.", HttpStatusCode.OK, oRootObject);
         }
+        else
+        {
+          MessageResponseUtility.FillPasswordRecoveryInfos("", " Can Not Add New Device.", HttpStatusCode.BadRequest, oRootObject);
+        }
+
+        response = MessageResponseUtility.PrepareJsonResponse<PasswordRecoveryRootObjectEntity>(oRootObject);
+      }
+      catch (Exception ex)
+      {
+        MessageResponseUtility.FillPasswordRecoveryInfos(string.Empty, ex.ToString(), HttpStatusCode.BadRequest, oRootObject);
+        response = MessageResponseUtility.PrepareJsonResponse<PasswordRecoveryRootObjectEntity>(oRootObject);
+      }
+
+      return response;
+    }
+    [Route("api/DeleteDevice")]
+    [HttpPost]
+    public HttpResponseMessage DeleteDevice(JObject encryptedString)
+    {
+      HttpResponseMessage response;
+      PasswordRecoveryRootObjectEntity oRootObject = new PasswordRecoveryRootObjectEntity();
+      try
+      {
+        #region Initialization
+
+        oRootObject.data = new PasswordRecoveryObjectEntity();
+        string msg = string.Empty;
+
+        msg = SecurityManager.Decrypt(encryptedString["encryptedString"].ToString());
+        if (string.IsNullOrEmpty(msg))
+        {
+          return response = Request.CreateResponse(HttpStatusCode.BadRequest, "Not have sufficient information to process.");
+        }
+
+        #endregion
+
+        DeviceDeleteJsonParser jsonManager = new DeviceDeleteJsonParser(msg, MessageReceivedFrom.DeleteDevice);
+        bool isSuccess = jsonManager.DeleteDevice();
+
+        if (isSuccess)
+        {
+          MessageResponseUtility.FillPasswordRecoveryInfos("", " Device Delete Successfully.", HttpStatusCode.OK, oRootObject);
+        }
+        else
+        {
+          MessageResponseUtility.FillPasswordRecoveryInfos("", " Can Not Delete Device.", HttpStatusCode.BadRequest, oRootObject);
+        }
+
+        response = MessageResponseUtility.PrepareJsonResponse<PasswordRecoveryRootObjectEntity>(oRootObject);
+      }
+      catch (Exception ex)
+      {
+        MessageResponseUtility.FillPasswordRecoveryInfos(string.Empty, ex.ToString(), HttpStatusCode.BadRequest, oRootObject);
+        response = MessageResponseUtility.PrepareJsonResponse<PasswordRecoveryRootObjectEntity>(oRootObject);
+      }
+
+      return response;
+    }
+
+    [Route("api/NewRouter")]
+    [HttpPost]
+    public HttpResponseMessage NewRouter(JObject encryptedString)
+    {
+      HttpResponseMessage response;
+      PasswordRecoveryRootObjectEntity oRootObject = new PasswordRecoveryRootObjectEntity();
+      try
+      {
+        #region Initialization
+
+        oRootObject.data = new PasswordRecoveryObjectEntity();
+        string msg = string.Empty;
+
+        msg = SecurityManager.Decrypt(encryptedString["encryptedString"].ToString());
+        if (string.IsNullOrEmpty(msg))
+        {
+          return response = Request.CreateResponse(HttpStatusCode.BadRequest, "Not have sufficient information to process.");
+        }
+
+        #endregion
+
+        RouterJsonParser jsonManager = new RouterJsonParser(msg, MessageReceivedFrom.NewRouter);
+        bool isSuccess = jsonManager.SaveNewRouter();
+
+        if (isSuccess)
+        {
+          MessageResponseUtility.FillPasswordRecoveryInfos("", " New Router Add Successfully.", HttpStatusCode.OK, oRootObject);
+        }
+        else
+        {
+          MessageResponseUtility.FillPasswordRecoveryInfos("", " Can Not Add New Router.", HttpStatusCode.BadRequest, oRootObject);
+        }
+
+        response = MessageResponseUtility.PrepareJsonResponse<PasswordRecoveryRootObjectEntity>(oRootObject);
+      }
+      catch (Exception ex)
+      {
+        MessageResponseUtility.FillPasswordRecoveryInfos(string.Empty, ex.ToString(), HttpStatusCode.BadRequest, oRootObject);
+        response = MessageResponseUtility.PrepareJsonResponse<PasswordRecoveryRootObjectEntity>(oRootObject);
+      }
+
+      return response;
+    }
 
 
     [Route("api/NewCameraConfigInfo")]
@@ -161,7 +161,7 @@ namespace SmartHome.WebAPI.Controllers
         string msg = string.Empty;
 
         msg = SecurityManager.Decrypt(encryptedString["encryptedString"].ToString());
-       // msg = "{\"Configuration\":\"CameraConfigInfo\",\"Home\":[{\"Id\":1,\"PassPhrase\":\"4905a51c1b987b8f_U4BXUO\"}],\"CameraConfigInfo\":[{\"Id\":1,\"Ip\":\"192.168.11.201\",\"Port\":8002,\"UserName\":\"smarthometest\",\"Password\":\"123456\",\"Home\":1,\"IsSynced\":0}]}";
+        // msg = "{\"Configuration\":\"CameraConfigInfo\",\"Home\":[{\"Id\":1,\"PassPhrase\":\"4905a51c1b987b8f_U4BXUO\"}],\"CameraConfigInfo\":[{\"Id\":1,\"Ip\":\"192.168.11.201\",\"Port\":8002,\"UserName\":\"smarthometest\",\"Password\":\"123456\",\"Home\":1,\"IsSynced\":0}]}";
         if (string.IsNullOrEmpty(msg))
         {
           return response = Request.CreateResponse(HttpStatusCode.BadRequest, "Not have sufficient information to process.");
---
+++ SmartHome.WebAPI/Filter/LogActionFilterAttribute.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json.Linq;
+using SmartHome.Logging;
+using SmartHome.Model.Enums;
+using SmartHome.Utility.EncryptionAndDecryption;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace SmartHome.WebAPI.Filter
+{
+  public class LogActionFilterAttribute : ActionFilterAttribute
+  {
+    public string msg { get; set; }
+
+
+    public override void OnActionExecuting(HttpActionContext filterContext)
+    {
+      msg = string.Empty;
+      string actionName = filterContext.ActionDescriptor.ActionName;
+      MessageReceivedFrom messageReceivedFrom = GetReceivedFrom(actionName);
+
+      JObject json = GetActionArgumentJson(filterContext);
+      msg = SecurityManager.Decrypt(json["encryptedString"].ToString());
+
+      new MessageLogger(msg, messageReceivedFrom).SaveNewMessageLog();
+    }
+
+    private JObject GetActionArgumentJson(HttpActionContext filterContext)
+    {
+      var encryptedString = filterContext.ActionArguments["encryptedString"].ToString();
+      JObject json = JObject.Parse(encryptedString);
+      return json;
+    }
+
+
+    public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
+    {
+      //base.OnActionExecuted(actionExecutedContext);
+      //Stopwatch stopwatch = (Stopwatch)actionExecutedContext.Request.Properties[actionExecutedContext.ActionContext.ActionDescriptor.ActionName];
+      //Trace.WriteLine(actionExecutedContext.ActionContext.ActionDescriptor.ActionName + "-Elapsed = " + stopwatch.Elapsed);
+    }
+
+    private MessageReceivedFrom GetReceivedFrom(string actionMethodName)
+    {
+      return (MessageReceivedFrom)System.Enum.Parse(typeof(MessageReceivedFrom), actionMethodName);
+    }
+  }
+}
